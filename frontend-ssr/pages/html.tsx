2d1daf92cc7a260547236dbd219bef20f8f4bbb4
Iampava <pava@iampava.com>
2020-07-30 21:04:17 +0300

    Added sass, global styles and CSS modules for rest of pages. Updated package.json

--- frontend-ssr/pages/html.tsx
+++ frontend-ssr/pages/html.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import styles from '../styles/pages/default-page.module.scss';
+
 export default function Html() {
   return (
-    <div className="container">
+    <div className={styles.page}>
       <Head>
         <title>HTML</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
+        <h1 className={styles.title}>
           Lectii HTML
         </h1>
         <Link href="/">
@@ -21,67 +23,6 @@ export default function Html() {
       <footer>
         Copyright {new Date().getFullYear()}
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          text-align: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }

2cceff25fc0fb73a70df68e3ce2f34e857f34e92
Iampava <pava@iampava.com>
2020-09-12 15:07:21 +0300

    <Lesson-0/> Content & implementation

--- frontend-ssr/.eslintrc.js
+++ frontend-ssr/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/no-unescaped-entities': 'off',
+    'react/require-default-props': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'import/extensions': 'off',

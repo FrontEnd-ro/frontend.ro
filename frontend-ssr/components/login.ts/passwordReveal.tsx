d391d98d5b5ca9e5b7a1d5c7a100623912a03781
SirCQQ <gatucristian@gmail.com>
2020-12-20 19:28:49 +0200

    Update passwordReveal.tsx

--- frontend-ssr/components/login.ts/passwordReveal.tsx
+++ frontend-ssr/components/login.ts/passwordReveal.tsx
@@ -1,41 +1,15 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import password from "./PasswordReveal.module.scss";
 import fromStyle from "./Form.module.scss";
 
-=======
-import password from "~/styles/pages/login/PasswordReveal.module.scss";
-import fromStyle from "~/styles/pages/login/Form.module.scss";
->>>>>>> 2f3a38f... Uncommited changes
-=======
-
->>>>>>> 55eb138...  Change in front, look over the git review
-=======
-import password from "~/styles/pages/login/PasswordReveal.module.scss";
-import fromStyle from "~/styles/pages/login/Form.module.scss";
->>>>>>> c8a07f9... Uncommited changes
-=======
-import password from "./PasswordReveal.module.scss";
-import fromStyle from "./Form.module.scss";
->>>>>>> 93cc1c0... For rebase
 const PasswordComponent = (props) => {
   const [visible, setVisible] = useState(false);
   const name = props.name || 'password';
   const autoComplete = props.autoComplete || 'on';
 
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div className={password["password-reveal"]}>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <input className="form__input" type={visible ? 'text' : 'password'}  name={name} autoComplete={autoComplete} value={props.value} onChange={props.onChange} />
-=======
-      <input className="form__input" type={visible ? 'text' : 'password'}  name={name} autoComplete={autoComplete} />
->>>>>>> 2f3a38f... Uncommited changes
       <button
         type="button"
         className={visible ? password['icon-eye'] : password['icon-eye-off']}
@@ -43,29 +17,6 @@ const PasswordComponent = (props) => {
         title={visible ? 'Hide password' : 'Show password'}
       />
       {props.password_error ?  <span className={fromStyle["error"]}>🧙‍ You shall not pass without a password.</span>  :null}
-=======
-    <div className="password-reveal">
-      <input className="form__input" type={visible ? 'text' : 'password'} required name={name} autoComplete={autoComplete} />
-=======
-    <div className={password["password-reveal"]}>
-      <input className="form__input" type={visible ? 'text' : 'password'}  name={name} autoComplete={autoComplete} />
->>>>>>> c8a07f9... Uncommited changes
-=======
-      <input className="form__input" type={visible ? 'text' : 'password'}  name={name} autoComplete={autoComplete} value={props.value} onChange={props.onChange} />
->>>>>>> 93cc1c0... For rebase
-      <button
-        type="button"
-        className={visible ? password['icon-eye'] : password['icon-eye-off']}
-        onClick={() => setVisible(!visible)}
-        title={visible ? 'Hide password' : 'Show password'}
-      />
-<<<<<<< HEAD
-      {console.log(props.children || props.reason )}
-      {props.children || !props.reason ? props.children : <span className="error">🧙‍ You shall not pass without a password.</span>}
->>>>>>> 55eb138...  Change in front, look over the git review
-=======
-      {props.password_error ?  <span className={fromStyle["error"]}>🧙‍ You shall not pass without a password.</span>  :null}
->>>>>>> c8a07f9... Uncommited changes
     </div>
   );
 };

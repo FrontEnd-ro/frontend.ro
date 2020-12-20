0ef2930bac9a14780a48a13d3619c0c33905b435
SirCQQ <gatucristian@gmail.com>
2020-12-20 19:42:35 +0200

    Adding card to auth

--- frontend-ssr/pages/index.tsx
+++ frontend-ssr/pages/index.tsx
@@ -47,19 +47,13 @@ export default function Home() {
             </a>
           </Link>
           
-          <Link href="/login">
+          <Link href="/auth">
             <a className="card">
               <h3>Login &rarr;</h3>
               <p>Login or Register</p>
             </a>
           </Link>
           
-          <Link href="/login">
-            <a className="card">
-              <h3>Login &rarr;</h3>
-              <p>Login or Register</p>
-            </a>
-          </Link>
         </div>
       </main>
 

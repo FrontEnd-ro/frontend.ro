894d50b1f2d04ecce0b83bbddfc05d362b452214
unknown <toma.andreeadiana@yahoo.com>
2020-12-31 09:52:12 +0200

    [HTMLTextsFundamentalsLesson] added
    [HTMLTextsFundamentalsLesson] SEO updated
    
    [HTMLTextsFundamentalsLesson] structure and semantic link added
    
    [HTMLTextsFundamentalsLesson] final touches
    
    [HTMLTextsFundamentalsLesson] final touches

--- frontend-ssr/services/contributors.ts
+++ frontend-ssr/services/contributors.ts
@@ -3,9 +3,8 @@ export const Pava = {
   avatar: '/images/contributors/iampava.png',
   url: 'https://iampava.com',
 };
-
-export const Ira = {
-  name: 'Ira',
-  avatar: '/images/contributors/iramelnic.jpg',
-  url: 'https://www.facebook.com/melnic.ira',
+export const Diana = {
+  name: 'Diana',
+  avatar: '/images/contributors/andreeatoma.jpg',
+  url: 'https://github.com/andreeatoma',
 };

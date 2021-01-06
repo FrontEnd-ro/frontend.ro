3b0a2447eadf5d8fe5fe51de4d7b84ce1e52b924
Iampava <pava@iampava.com>
2021-01-06 11:23:42 +0200

    [BUGFIX] Wrong path to Login component

--- frontend-ssr/pages/auth.tsx
+++ frontend-ssr/pages/auth.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Login from '../components/Login';
+import Login from '../components/login';
 
 export default function Authpage() {
   return (

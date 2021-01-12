b6eab65ccc044d42f5cb95313d920c38e1e4ac93
unknown <toma.andreeadiana@yahoo.com>
2021-01-12 13:11:33 +0200

    [Lessons] Lists

---
+++ frontend-ssr/pages/demo/html/liste-de-descrieri.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Demo, { DemoPreview } from '~/components/demo';
+import Header from '~/components/Header';
+import Footer from '~/components/Footer';
+import Highlight from '~/components/Highlight/Highlight';
+
+export default function DescriptionLists() {
+  const title = 'Liste de descrieri';
+
+  return (
+    <>
+      <Head>
+        <title>
+          {title}
+          {' '}
+          | FrontEnd.ro
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <>
+        <Header demoPage />
+        <Demo title={title}>
+          <p>
+            Întâlnite mai rar decât listele ordonate sau cele neordonate, pe acestea le folosim
+            când vrem să definim termeni sau să asociem perechi nume/descriere.
+          </p>
+          <DemoPreview>
+            <dl>
+              <dt> HTML </dt>
+              <dd> Hyper Text Markup Language </dd>
+
+              <dt> CSS </dt>
+              <dd> Cascading Style Sheets </dd>
+            </dl>
+          </DemoPreview>
+          <Highlight
+            className="my-5"
+            language="html"
+            code={`
+<dl>
+  <dt> HTML </dt>
+  <dd> Hyper Text Markup Language </dd>
+
+  <dt> CSS </dt>
+  <dd> Cascading Style Sheets </dd>
+</dl>
+                   
+`}
+          />
+        </Demo>
+        <Footer />
+      </>
+    </>
+  );
+}
---
+++ frontend-ssr/pages/demo/html/liste-ordonate.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Demo, { DemoPreview } from '~/components/demo';
+import Header from '~/components/Header';
+import Footer from '~/components/Footer';
+import Highlight from '~/components/Highlight/Highlight';
+
+export default function OrderedLists() {
+  const title = ' Liste ordonate';
+  return (
+    <>
+      <Head>
+        <title>
+          {title}
+          {' '}
+          | FrontEnd.ro
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <>
+        <Header demoPage />
+        <Demo title={title}>
+          <p>
+            Așa cum sugerează și numele lor, ordinea elementelor din cadrul
+            acestora este importantă.
+          </p>
+          <DemoPreview>
+            <ol>
+              <li> Cumpără ingredientele necesare </li>
+              <li> Amestecă ingredientele </li>
+              <li> Pune amestecul într-un vas de gătit </li>
+              <li> Coace în cuptor prăjitura timp de o oră </li>
+              <li> Scoate prăjitura din cuptor </li>
+            </ol>
+          </DemoPreview>
+          <Highlight
+            className="my-5"
+            language="html"
+            code={`
+<ol>
+  <li> Cumpără ingredientele necesare </li>
+  <li> Amestecă ingredientele </li>
+  <li> Pune amestecul într-un vas de gătit </li>
+  <li> Coace în cuptor prăjitura timp de o oră </li>
+  <li> Scoate prăjitura din cuptor </li>
+</ol>          
+`}
+          />
+        </Demo>
+        <Footer />
+      </>
+    </>
+  );
+}

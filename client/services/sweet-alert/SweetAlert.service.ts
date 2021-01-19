0f20d895a3c64f187f833a32b539ffbedc2f4011
Iampava <pava@iampava.com>
2021-01-19 19:30:16 +0200

    [SweetAlert] Display React component inside modal

--- client/services/sweet-alert/SweetAlert.service.ts
+++ client/services/sweet-alert/SweetAlert.service.ts
@@ -1,3 +1,4 @@
+import React, { ReactElement } from 'react';
 import styles from './SweetAlert.module.scss';
 
 let Toast = null;
@@ -13,10 +14,7 @@ class SweetAlertService {
   private static anchorTag: HTMLAnchorElement;
 
   static toast({
-    text,
-    href = null,
-    timer = 3000,
-    type = 'success',
+    text, href = null, timer = 3000, type = 'success',
   }: ToastI) {
     SweetAlertService.init();
 
@@ -26,7 +24,7 @@ class SweetAlertService {
       target = SweetAlertService.anchorTag;
     }
 
-    return new Promise((resolve) => {
+    return new Promise<void>((resolve) => {
       if (Toast !== null) {
         return resolve();
       }
@@ -47,11 +45,37 @@ class SweetAlertService {
     }).then(() => {
       Toast.fire({
         icon: type,
-        title: type === 'error' ? text || 'Oups, something went wrong...' : text,
+        title:
+          type === 'error' ? text || 'Oups, something went wrong...' : text,
         target,
         timer,
       });
     });
+  }
+
+  static async content(ReactComponent: any, title = null, props = {}) {
+    const [contentModule, swalModule] = await Promise.all([
+      import('sweetalert2-react-content'),
+      import('sweetalert2'),
+    ]);
+
+    const ReactSwal = contentModule.default(swalModule.default);
+
+    return ReactSwal.fire({
+      title,
+      html: React.createElement(ReactComponent, {
+        closeModal: () => swalModule.default.close(),
+        ...props,
+      }),
+      customClass: styles['swal2-content'],
+      showConfirmButton: false,
+      heightAuto: false,
+    });
+  }
+
+  static async closePopup() {
+    const swal = await import('sweetalert2');
+    swal.default.close();
   }
 
   private static init() {

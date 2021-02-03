1f6653e5479f329537a12098138befc68805b9be
Iampava <pava@iampava.com>
2021-02-03 11:18:13 +0200

    [ExercisePreview] Main component & skeleton

---
+++ client/components/ExercisePreview/ExercisePreview.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock, faPlay } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './ExercisePreview.module.scss';
+import Markdown from '../Markdown';
+
+export type ExerciseViewMode = 'STUDENT' | 'TEACHER'
+
+interface Props {
+  href: string;
+  body: string;
+  tags: string[];
+  viewMode: ExerciseViewMode;
+  isPrivate: boolean;
+  author: {
+    username: string;
+    avatar: string;
+  }
+  feedbackCount: number;
+  isApproved: boolean;
+
+  // Do we need this?
+  readOnly: boolean;
+}
+
+function ExercisePreview({
+  href,
+  body,
+  viewMode,
+  isPrivate,
+  author,
+  readOnly,
+  feedbackCount,
+  isApproved,
+  tags,
+}: Props) {
+  const { btnText, infoMessage } = getFooterTexts(viewMode, feedbackCount, readOnly, isApproved);
+
+  let className = `${styles['exercise-preview']} bg-white rounded-md`;
+  if (isApproved) {
+    className += ` ${styles['is--done']}`;
+  }
+  if (!isApproved && readOnly) {
+    className += ` ${styles['is--waiting']}`;
+  }
+  if (feedbackCount > 0) {
+    className += ` ${styles['has--issues']}`;
+  }
+
+  return (
+    <div className={className}>
+      <header className="text-right">
+        {isPrivate && <FontAwesomeIcon className="text-grey" icon={faLock} title="Exercițiu privat" />}
+      </header>
+      <Markdown
+        className={`${styles.body} relative overflow-hidden`}
+        markdownString={body}
+        variant="transparent"
+      />
+      <footer className="d-flex align-items-center justify-content-between">
+        <>
+          {viewMode === 'STUDENT' && infoMessage && (
+            <span>
+              {infoMessage}
+            </span>
+          )}
+          {(viewMode === 'TEACHER' || !infoMessage) && (
+            <>
+              <Link href={`/${author.username}`}>
+                <a className={styles.avatar}>
+                  <img src={author.avatar} alt="Author avatar" />
+                </a>
+              </Link>
+              <div className={`${styles.tags} truncate d-inline-block`}>
+                {tags.map((t) => (
+                  <span className="text-bold" key={t}>
+                    {t}
+                  </span>
+                ))}
+              </div>
+            </>
+          )}
+        </>
+        <Link href={href}>
+          <a className={`btn no-underline ${isApproved || readOnly || feedbackCount > 0 ? 'btn--light' : 'btn--blue'}`}>
+            {btnText}
+            <FontAwesomeIcon className="ml-2" icon={faPlay} />
+          </a>
+        </Link>
+      </footer>
+    </div>
+  );
+}
+
+function getFooterTexts(
+  viewMode: ExerciseViewMode,
+  feedbackCount: number,
+  readOnly: boolean,
+  isApproved: boolean,
+) {
+  let btnText = viewMode === 'STUDENT' ? 'Rezolvă' : 'Editează';
+  let infoMessage = '';
+
+  if (viewMode === 'STUDENT') {
+    if (readOnly || isApproved) {
+      btnText = 'Vezi exercițiul';
+    } else if (feedbackCount) {
+      btnText = 'Continuă';
+    }
+
+    if (isApproved) {
+      infoMessage = '✔';
+    } else if (feedbackCount) {
+      infoMessage = `${feedbackCount} problem${feedbackCount > 1 ? 'e' : 'ă'} de rezolvat`;
+    } else if (readOnly) {
+      infoMessage = 'În așteptarea feedback-ului';
+    }
+  }
+
+  return {
+    btnText,
+    infoMessage,
+  };
+}
+
+export default ExercisePreview;
---
+++ client/components/Markdown/Markdown.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import styles from './Markdown.module.scss';
+
+interface Props {
+  markdownString: string;
+  variant?: 'none' | 'transparent';
+  className?: string;
+}
+
+function Markdown({ markdownString, className = '', variant = 'none' }: Props) {
+  const markdownRef = useRef(null);
+
+  useEffect(() => {
+    import('marked').then((module) => {
+      const marked = module.default;
+
+      marked.setOptions({
+        sanitize: true,
+      });
+
+      markdownRef.current.innerHTML = marked(markdownString);
+    });
+  }, []);
+
+  return (
+    <div className={`${styles.markdown} ${variant === 'transparent' && styles['is--transparent']} ${className}`} ref={markdownRef} />
+  );
+}
+
+export default Markdown;

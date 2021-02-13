5ebe271e92f2d8e61bb1601e916169ee3f6264aa
Iampava <pava@iampava.com>
2021-02-13 12:25:14 +0200

    [User] Added role property to users. It can be one of: admin, teacher or student

--- shared/SharedConstants.js
+++ shared/SharedConstants.js
@@ -2,9 +2,16 @@
 const MAX_MEDIA_MB = 10;
 const MAX_MEDIA_BYTES = MAX_MEDIA_MB * 1024 * 1024;
 
+const USER_ROLE = {
+  ADMIN: 'admin',
+  TEACHER: 'teacher',
+  STUDENT: 'student',
+};
+
 // Use NodeJS style exports because this file is consumed
 // both from the server & client
 module.exports = {
   MAX_MEDIA_MB,
   MAX_MEDIA_BYTES,
+  USER_ROLE,
 };

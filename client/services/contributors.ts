5f56c05376e63f5d603bcaa17296668090e2eca0
Iampava <pava@iampava.com>
2021-03-08 07:53:21 +0200

    [Events] Copyright information

--- client/services/contributors.ts
+++ client/services/contributors.ts
@@ -9,13 +9,13 @@ export const Pava : Contributor = {
   name: 'Păvă',
   avatar: '/images/contributors/iampava.png',
   url: 'https://iampava.com',
-  shortBio: 'Founder at FrontEnd.ro',
+  shortBio: 'Fondatorul FrontEnd.ro',
 };
 export const Pava2: Contributor = {
   name: 'Păvă',
   avatar: '/images/contributors/iampava_2.jpg',
   url: 'https://iampava.com',
-  shortBio: 'Founder at FrontEnd.ro',
+  shortBio: 'Fondatorul FrontEnd.ro',
 };
 export const Diana : Contributor = {
   name: 'Diana',
@@ -36,7 +36,7 @@ export const CatalinPopusoi : Contributor = {
   name: 'Cătălin',
   avatar: '/images/contributors/catalinpopusoi.jpg',
   url: 'https://github.com/catalinpopusoi',
-  shortBio: 'FullStack developer at Endava',
+  shortBio: 'FrontEnd developer la Endava',
 };
 export const RobertParasca : Contributor = {
   name: 'Robert',
@@ -57,5 +57,5 @@ export const GeorgeMarianMorosac : Contributor = {
   name: 'George',
   avatar: '/images/contributors/georgemarianmorosac.jpg',
   url: 'https://www.linkedin.com/in/george-marian-morosac-7381508a/',
-  shortBio: 'Full Stack developer at Amazon',
+  shortBio: 'SDE la Amazon',
 };

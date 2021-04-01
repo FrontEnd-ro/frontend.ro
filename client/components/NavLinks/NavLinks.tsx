796e834e29adf29e580c49505d4666808367ee9a
Iampava <pava@iampava.com>
2021-04-01 10:40:50 +0300

    [NavLinks] If you're logged in show logout button

--- client/components/NavLinks/NavLinks.tsx
+++ client/components/NavLinks/NavLinks.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from '~/redux/root.reducer';
+import UserService from '~/services/User.service';
+import { logoutUser } from '~/redux/user/user.actions';
+import Button from '~/components/Button';
+
 import styles from './NavLinks.module.scss';
 
-function NavLinks() {
+function NavLinks({ user, dispatch }: ConnectedProps<typeof connector>) {
+  const router = useRouter();
+  const isLoggedIn = !!user.info;
+
+  const logout = () => {
+    UserService.logout().then(() => {
+      router.replace('/').then(() => {
+        dispatch(logoutUser());
+      });
+    });
+  };
+
   return (
     <nav className={styles['nav-links']}>
       <ol>
@@ -34,16 +52,32 @@ function NavLinks() {
             </a>
           </Link>
         </li>
-        <li className={styles.login}>
-          <Link href="/auth">
-            <a>
-              Intră în cont
-            </a>
-          </Link>
-        </li>
+        {isLoggedIn ? (
+          <li className={styles.login}>
+            <Button variant="transparent" onClick={logout}>
+              Sign out
+            </Button>
+          </li>
+        ) : (
+          <li className={styles.login}>
+            <Link href={`/auth?next=${encodeURIComponent(router.asPath)}`}>
+              <a>
+                Intră în cont
+              </a>
+            </Link>
+          </li>
+        )}
       </ol>
     </nav>
   );
 }
 
-export default NavLinks;
+function mapStateToProps(state: RootState) {
+  return {
+    user: state.user,
+  };
+}
+
+const connector = connect(mapStateToProps);
+
+export default connector(NavLinks);

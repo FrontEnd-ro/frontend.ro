a4c565bf3fc60d7b2645c575d8765d1705500258
Diana Toma <toma.andreeadiana@yahoo.com>
2021-04-09 16:19:46 +0300

    [Storybook] stories for <Header>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
 import '!style-loader!css-loader!sass-loader!../client/styles/index.scss';
+// @ config.js
+import { configure,  addDecorator } from '@storybook/react';
+
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../client/redux/root.reducer';
+
+import { withNextRouter } from 'storybook-addon-next-router';
+
+export const decorators = [withNextRouter()];
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +21,13 @@ export const parameters = {
     },
   },
 }
+
+const store = createStore(rootReducer);
+
+addDecorator(S => (
+  <Provider store={store}>
+    <S />
+  </Provider>
+));
+
+configure(require.context('../client', true, /\.stories\.js$/), module);
---
+++ client/components/Header/Header.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+export default {
+  title: 'Header',
+};
+
+export const HeaderWithNavMenu = () => (
+  <Header withNavMenu />
+);
+
+export const HeaderDemoPage = () => (
+  <Header demoPage />
+);

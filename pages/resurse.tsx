44e45232eabe83f93bffb0e00d46c60e57b130d6
Iampava <pava@iampava.com>
2021-06-02 12:52:09 +0300

    [SEO] Share image for resources page

--- pages/resurse.tsx
+++ pages/resurse.tsx
@@ -9,7 +9,7 @@ function ResourcesPage() {
     <>
       <SEOTags
         title="Resurse pentru fiecare FrontEnd developer | FrontEnd.ro"
-        shareImage="https://frontend.ro/learn-seo-image.jpg"
+        shareImage={`${process.env.CLOUDFRONT_PUBLIC}/public/seo/resources.jpg`}
         description="Am pornit de la site-urile ce ne ajută zilnic în joburile nostre de FrontEnd developeri, le-am păstrat doar pe cele mai utile și am creat cu ele lista de aici."
         url="https://FrontEnd.ro/resurse"
       />

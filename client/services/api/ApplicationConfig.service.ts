a268151b106f707e645b4e5648bf424577ae9384
Iampava <pava@iampava.com>
2021-09-30 20:27:39 +0300

    Configure the '/application-config' request so it doesn't show the SWAL popup in case it fails (https://github.com/FrontEnd-ro/frontend.ro/issues/355)

--- client/services/api/ApplicationConfig.service.ts
+++ client/services/api/ApplicationConfig.service.ts
@@ -3,7 +3,7 @@ import HttpService from '../Http.service';
 class ApplicationConfigService {
   static get() {
     return HttpService
-      .get(`${process.env.ENDPOINT}/application-config`)
+      .get(`${process.env.ENDPOINT}/application-config`, undefined, true)
       .then((resp) => resp.json());
   }
 }

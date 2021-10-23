a2b39b1a8ed7a2d01ca2952374927c3a56ddef11
Iampava <pava@iampava.com>
2021-10-23 13:26:09 +0300

    Refactor Email.service.js to TypeScript (https://github.com/FrontEnd-ro/frontend.ro/issues/387)

--- server/Email.service.ts
+++ server/Email.service.ts
@@ -3,7 +3,11 @@ const postmark = require('postmark');
 const client = new postmark.ServerClient(process.env.EMAIL_TOKEN);
 
 
-function sendTemplateWithAlias(email, TemplateAlias, TemplateModel) {
+function sendTemplateWithAlias(
+  email: string,
+  TemplateAlias: string,
+  TemplateModel: Record<string, any>
+) {
   const options = {
     To: process.env.APP_ENV === 'production' ? email : 'pava@frontend.ro',
     From: 'hello@frontend.ro',

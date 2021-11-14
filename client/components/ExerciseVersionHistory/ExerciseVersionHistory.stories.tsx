3964241ca5bf6177c8ef1301f89ad33987c125a6
Iampava <pava@iampava.com>
2021-11-14 15:47:23 +0200

    Create new <ExerciseVersion> component (https://github.com/FrontEnd-ro/frontend.ro/issues/409)

---
+++ client/components/ExerciseVersionHistory/ExerciseVersionHistory.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ExerciseVersion as ExerciseVersionComponent } from './ExerciseVersionHistory';
+import { FeedbackType, SubmissionVersionI } from '~/../shared/types/submission.types';
+
+export default {
+  title: 'Exercise/Version',
+  component: ExerciseVersionComponent,
+} as ComponentMeta<typeof ExerciseVersionComponent>;
+
+const Template: ComponentStory<typeof ExerciseVersionComponent> = (args) => (
+  <ExerciseVersionComponent {...args} />
+
+);
+
+const approvedVersion: SubmissionVersionI = {
+  _id: '_id',
+  approved: true,
+  feedbacks: [],
+  code: 'code',
+  createdAt: '11 March 2021',
+  submission: 'submission_id',
+};
+
+const rejectedVersion: SubmissionVersionI = {
+  _id: '_id',
+  approved: false,
+  feedbacks: [{
+    type: FeedbackType.IMPROVEMENT,
+    file_key: 'file_key',
+    body: 'body',
+    position: [1, 2, 3, 4],
+  }, {
+    type: FeedbackType.OPINION,
+    file_key: 'file_key',
+    body: 'body',
+    position: [1, 2, 3, 4],
+  }, {
+    type: FeedbackType.PRAISE,
+    file_key: 'file_key',
+    body: 'body',
+    position: [1, 2, 3, 4],
+  }],
+  code: 'code',
+  createdAt: '11 March 2021',
+  submission: 'submission_id',
+};
+
+export const Approved = Template.bind({});
+Approved.args = {
+  href: '#',
+  index: 1,
+  version: approvedVersion,
+};
+
+export const ApprovedActive = Template.bind({});
+ApprovedActive.args = {
+  href: '#',
+  index: 1,
+  isActive: true,
+  version: approvedVersion,
+};
+
+export const Rejected = Template.bind({});
+Rejected.args = {
+  href: '#',
+  index: 2,
+  version: rejectedVersion,
+};
---
+++ client/components/ExerciseVersionHistory/ExerciseVersionHistory.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { UrlObject } from 'url';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import List from '../List';
+import { timeAgo } from '~/services/Utils';
+import RoutingUtils from '~/services/utils/Routing.utils';
+import { SubmissionVersionI } from '~/../shared/types/submission.types';
+
+import styles from './ExerciseVersionHistory.module.scss';
+
+interface ExerciseVersionHistoryProps {
+  versions: SubmissionVersionI[];
+  hrefBuilder: (v: SubmissionVersionI) => UrlObject;
+}
+
+const ExerciseVersionHistory = ({ versions, hrefBuilder }: ExerciseVersionHistoryProps) => {
+  const router = useRouter();
+
+  if (!versions.length) {
+    return null;
+  }
+
+  return (
+    <details className="pb-5">
+      <summary className="text-xl cursor-pointer">
+        Istoricul exercițiului
+      </summary>
+      <nav>
+        <List>
+          {versions.map((v, index) => (
+            <li className="my-5" key={v._id}>
+              <ExerciseVersion
+                version={v}
+                href={hrefBuilder(v)}
+                index={versions.length - index}
+                isActive={RoutingUtils.isRouteActive(router, hrefBuilder(v))}
+              />
+            </li>
+          ))}
+        </List>
+      </nav>
+    </details>
+  );
+};
+
+interface ExerciseVersionProps {
+  index: number;
+  href: string | UrlObject;
+  version: SubmissionVersionI;
+  isActive: boolean;
+  className?: string;
+}
+
+const ExerciseVersion = ({
+  index,
+  href,
+  version,
+  isActive,
+  className = '',
+}: ExerciseVersionProps) => {
+  const dateTime = format(new Date(version.createdAt).getTime(), 'yyyy-MM-dd');
+
+  return (
+    <Link replace href={href} passHref>
+      <a className={`
+        ${className}
+        ${styles.ExerciseVersion} 
+        ${isActive ? styles['ExerciseVersion--active'] : ''}
+        d-flex no-underline
+        `}
+      >
+        <div className={`${styles.index} d-flex p-3 align-items-center text-2xl`}>
+          {index}
+        </div>
+        <div className="p-3">
+          <p className="text-bold m-0 d-flex align-items-center">
+            {version.approved ? (
+              <>
+                <span className="mr-2"> Soluție corectă </span>
+                <FontAwesomeIcon width="16" icon={faCheck} />
+              </>
+            ) : (
+              <>
+                <span className="mr-2"> Soluție respinsă </span>
+                <FontAwesomeIcon width="16" icon={faTimes} />
+              </>
+            )}
+          </p>
+          <time title={dateTime} className="d-block mt-4 mb-2" dateTime={dateTime}>
+            <FontAwesomeIcon width="16" className="text-silver" icon={faCalendarAlt} />
+            {' '}
+            {timeAgo(new Date(version.createdAt))}
+          </time>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default ExerciseVersionHistory;
+export { ExerciseVersion };

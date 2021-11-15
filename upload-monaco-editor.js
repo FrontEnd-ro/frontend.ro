4e417b95a72c4fc741dd834e08a92a31df03047c
Iampava <pava@iampava.com>
2021-11-15 22:15:32 +0200

    Upgrade monaco to v0.30.1 (https://github.com/FrontEnd-ro/frontend.ro/issues/413)

---
+++ upload-monaco-editor.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// Have a look at README.md (the Monaco Editor section)
+// to understand why we need this GitHub Action.
+const { spawn } = require('child_process');
+const packageJson = require('./package.json');
+
+const monacoVersion = packageJson.dependencies['monaco-editor'];
+
+// https://docs.aws.amazon.com/cli/latest/reference/s3/index.html
+spawn('aws', [
+  's3',
+  'cp',
+  '--region',
+  'eu-west-1',
+  'node_modules/monaco-editor/min/',
+  `${process.env.BASE_S3_URI}/lib/monaco-editor/${monacoVersion}`,
+  '--recursive',
+], { stdio: 'inherit' });

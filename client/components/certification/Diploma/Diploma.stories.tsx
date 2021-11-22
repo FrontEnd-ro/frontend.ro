4aa9704c846330212533a1fe2db30dec0f74fa73
Iampava <pava@iampava.com>
2021-11-22 21:53:05 +0200

    Create a generic <Diploma> component to be re-used for certifications (https://github.com/FrontEnd-ro/frontend.ro/issues/431)

---
+++ client/components/certification/Diploma/Diploma.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import DiplomaComponent from './Diploma';
+
+export default {
+  title: 'Certification/Diploma',
+  component: DiplomaComponent,
+} as ComponentMeta<typeof DiplomaComponent>;
+
+const Template: ComponentStory<typeof DiplomaComponent> = (args) => (
+  <div style={{
+    margin: '2em auto',
+    width: '90%',
+    maxWidth: '1200px',
+  }}
+  >
+    <DiplomaComponent {...args} />
+  </div>
+);
+
+const baseArgs = {
+  student: {
+    name: 'Jon Doe',
+    username: 'jondoe',
+    avatar: 'https://iampava.com/images/pava.jpg',
+  },
+  module: {
+    name: 'Modulul de HTML',
+    url: '#html',
+  },
+  certification: {
+    date: new Date(),
+    exerciseCount: 18,
+    url: 'https://frontend.ro',
+  },
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  ...baseArgs,
+  variant: 'large',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  ...baseArgs,
+  variant: 'medium',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  ...baseArgs,
+  variant: 'small',
+};
---
+++ client/components/certification/Diploma/Diploma.tsx
@@ -0,0 +1,134 @@
+import React, { useRef } from 'react';
+import { format } from 'date-fns';
+import { formatDate } from '~/services/Utils';
+import { ShareButton } from '~/components/SocialMediaButtons';
+
+import styles from './Diploma.module.scss';
+
+interface Props {
+  student: {
+    name: string;
+    username: string;
+    avatar: string;
+  }
+  module: {
+    name: string;
+    url: string;
+  },
+  certification: {
+    date: Date;
+    url: string;
+    exerciseCount: number;
+  },
+  variant: 'large' | 'medium' | 'small'
+}
+
+const Diploma = ({
+  student, module, certification, variant = 'large',
+}: Props) => (
+  <section className={`${styles.Diploma} ${styles[`Diploma-${variant}`]} relative`}>
+    <time className="absolute d-block mb-4" dateTime={format(certification.date.getTime(), 'yyyy-MM-dd')}>
+      {formatDate(certification.date)}
+    </time>
+    <div className={`${styles.main} text-center`}>
+      <a
+        href={`/${student.username}`}
+        className={`${styles.student} no-underline inline-flex align-items-center text-left`}
+      >
+        <div className={`${styles['student-info']} mx-4`}>
+          <p className="m-0 text-bold">{student.name}</p>
+          <p className={`${styles.username} text-xs m-0 truncate`}>
+            {`@${student.username}`}
+          </p>
+        </div>
+        <img
+          width="200"
+          height="200"
+          src={student.avatar}
+          alt={`${student.name} avatar`}
+        />
+      </a>
+      <p>
+        a completat
+        <a className="mt-4 mb-4 d-block text-bold" href={module.url}>
+          {module.name}
+        </a>
+        rezolvând cu succes toate cele
+        {' '}
+        {certification.exerciseCount}
+        {' '}
+        exerciții.
+      </p>
+    </div>
+    <footer className="relative d-flex justify-content-between align-items-end">
+      <Signature className="mr-2" />
+      <QRCode
+        width={140}
+        background="#f8f8f8"
+        className={styles.qrcode}
+        url={certification.url}
+      />
+      <div className={`${styles['share-button-wrapper']} absolute`}>
+        <ShareButton
+          url={certification.url}
+          config={{
+            copy: true,
+            facebook: true,
+            linkedin: true,
+          }}
+          direction="up"
+        />
+      </div>
+    </footer>
+  </section>
+);
+
+const Signature = ({ className = '' }: { className?: string }) => {
+  return (
+    <div className={`${styles.signature} ${className}`}>
+      <img
+        height="100"
+        alt="Founder Signature"
+        className="border-bottom-1px"
+        src={`${process.env.CLOUDFRONT_PUBLIC}/public/images/diploma-signature.png`}
+      />
+      <p className="m-0">
+        Alexandru Păvăloi,
+        <br />
+        Fondatorul FrontEnd.ro
+      </p>
+    </div>
+  );
+};
+
+const QRCode = ({
+  url,
+  width,
+  className = '',
+  background = '#fff',
+}: {
+  url: string;
+  width: number;
+  background?: string;
+  className?: string;
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  import('qrcode').then((module) => {
+    module.default.toCanvas(canvasRef.current, url, {
+      color: {
+        light: background,
+      },
+      width,
+    }, (error) => {
+      if (error) console.error(error);
+      console.log('success!');
+    });
+  });
+
+  return (
+    <canvas className={className} ref={canvasRef} />
+  );
+};
+
+export default Diploma;

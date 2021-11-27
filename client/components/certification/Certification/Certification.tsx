e7c4a2be54bb4abbec7a19337e18669033f35818
Iampava <pava@iampava.com>
2021-11-27 13:16:04 +0200

    Create new public route for viewing a certification (https://github.com/FrontEnd-ro/frontend.ro/issues/436)

---
+++ client/components/certification/Certification/Certification.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import Link from 'next/link';
+import Diploma from '../Diploma/Diploma';
+import ExercisePreview from '~/components/ExercisePreview';
+import { WIPPopulatedCertificationI } from '~/../shared/types/certification.types';
+
+import styles from './Certification.module.scss';
+import List from '~/components/List';
+
+const Certification = ({ certification }: { certification: WIPPopulatedCertificationI }) => {
+  const nameOrUsername = certification.user.name ?? certification.user.username;
+
+  return (
+    <section className={styles.Certification}>
+      <Diploma
+        student={certification.user}
+        module={certification.module}
+        certification={{
+          date: new Date(certification.timestamp),
+          exerciseCount: certification.lesson_exercises.length,
+          url: `/certificari/${certification._id}`,
+        }}
+      />
+      <p className="mt-8 mb-12 text-2xl font-light">
+        <Link href={`/${certification.user.username}`}>
+          <a className="border-bottom-1px no-underline">
+            {nameOrUsername}
+          </a>
+        </Link>
+        {' '}
+        a rezolvat cu succes toate cele
+        {' '}
+        <span className="text-bold">
+          {certification.lesson_exercises.length}
+          {' '}
+          exerciții
+        </span>
+        {' '}
+        ale acestui modul. Fiecare exercițiu a fost
+        trimis prin platforma
+        {' '}
+        <Link href="/">
+          <a className="border-bottom-1px no-underline">
+            FrontEnd.ro
+          </a>
+        </Link>
+        {' '}
+        și evaluat de echipa noastră.
+      </p>
+      <h2>
+        Exerciții rezolvate corect de
+        {' '}
+        {nameOrUsername}
+      </h2>
+      <List className={styles['exercise-list']}>
+        {certification.lesson_exercises.map((lessonExercise) => (
+          <li key={lessonExercise._id}>
+            <ExercisePreview
+              exercise={lessonExercise}
+              isPrivate={false}
+              viewMode="STUDENT"
+              feedbackCount={0}
+              isApproved={false}
+              readOnly={false}
+              href={`/rezolva/${lessonExercise._id}`}
+            />
+          </li>
+        ))}
+      </List>
+    </section>
+  );
+};
+
+export default Certification;
--- shared/types/certification.types.ts
+++ shared/types/certification.types.ts
@@ -1,4 +1,5 @@
-import { PopulatedDoc } from 'mongoose';
+import { PopulatedDoc, Document } from 'mongoose';
+import { LessonExerciseI, WIPPopulatedLessonExerciseI } from './exercise.types';
 import { UserI } from './user.types';
 
 export interface CertificationModule {
@@ -19,7 +20,24 @@ export interface CertificationI {
   // Nanos
   timestamp: number;
 
-  // IDs of Lesson Exercises
+  // UUIDs of Lesson Exercises
   // eslint-disable-next-line camelcase
-  lesson_exercises: string[];
+  lesson_exercises: PopulatedDoc<LessonExerciseI>[];
+}
+
+// FIXME: https://github.com/FrontEnd-ro/frontend.ro/issues/443
+export interface WIPPopulatedCertificationI {
+  // TODO: what is the correct way of defining IDs?
+  // https://github.com/FrontEnd-ro/frontend.ro/issues/442
+  _id?: string;
+
+  module: CertificationModule;
+
+  user: UserI;
+
+  // Nanos
+  timestamp: number;
+
+  // eslint-disable-next-line camelcase
+  lesson_exercises: WIPPopulatedLessonExerciseI[];
 }

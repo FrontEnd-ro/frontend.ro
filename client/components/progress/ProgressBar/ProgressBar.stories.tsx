802d74d3a8925e2446933c5756ed625d2582c7ae
Iampava <pava@iampava.com>
2021-12-14 09:15:36 +0200

    Create a new ProgressBar component, with both vertical and horizontal variants (https://github.com/FrontEnd-ro/frontend.ro/issues/480)

---
+++ client/components/progress/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ProgressBarComponent from './ProgressBar';
+
+export default {
+  title: 'ProgressBar',
+  component: ProgressBarComponent,
+} as ComponentMeta<typeof ProgressBarComponent>;
+
+const Template: ComponentStory<typeof ProgressBarComponent> = (args) => (
+  <div style={{
+    margin: '2em auto',
+    width: '90%',
+    height: '500px',
+    maxWidth: '1200px',
+  }}
+  >
+    <ProgressBarComponent {...args} />
+  </div>
+);
+
+const baseArgs = {
+  count: 10,
+  items: [{
+    count: 2,
+    color: 'green',
+  }, {
+    count: 5,
+    color: 'yellow',
+  }],
+};
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+  ...baseArgs,
+  direction: 'horizontal',
+};
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  ...baseArgs,
+  direction: 'vertical',
+};
---
+++ client/components/progress/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from './ProgressBar.module.scss';
+
+interface Props {
+  count: number;
+  items: ProgressItem[];
+  className?: string;
+  direction?: Direction;
+}
+
+type Direction = 'horizontal' | 'vertical';
+
+export interface ProgressItem {
+  count: number;
+  color: string;
+}
+
+const ProgressBar = ({
+  direction = 'horizontal', count, items, className = '',
+}: Props) => {
+  const totalItemsCount = items.reduce((acc, cur) => cur.count + acc, 0);
+
+  return (
+    <div
+      // A11y: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_progressbar_role„
+      role="progressbar"
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-valuenow={(totalItemsCount / count) * 100}
+      className={`
+        ${className}
+        ${styles.ProgressBar}
+        ${styles[`ProgressBar-${direction}`]}
+        ${totalItemsCount === count ? styles['ProgressBar-full'] : ''}
+      `}
+    >
+      {items.map((item, index) => (
+        <ProgressItem
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          color={item.color}
+          direction={direction}
+          percentage={(item.count * 100) / count}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProgressItem = ({
+  direction,
+  color,
+  percentage,
+}: { direction: Direction, color: string, percentage: number }) => {
+  const style = direction === 'horizontal' ? {
+    background: color,
+    width: `${percentage}%`,
+  } : {
+    background: color,
+    height: `${percentage}%`,
+  };
+
+  return (
+    <div className={styles.ProgressItem} style={style} />
+  );
+};
+
+export default ProgressBar;

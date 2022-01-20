b98ee035283d75106abaae2340ec5a6aeffa2947
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-20 13:46:56 +0200

    Create new landing for the HTML Module page (https://github.com/FrontEnd-ro/frontend.ro/issues/367)

---
+++ client/components/HtmlLanding/HtmlLanding.tsx
@@ -0,0 +1,224 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useInView } from 'react-intersection-observer';
+import { connect, ConnectedProps } from 'react-redux';
+import List from '../List';
+import Header from '../Header';
+import Button from '../Button';
+import Login from '~/components/login/Login';
+import { RootState } from '~/redux/root.reducer';
+import { withSmoothScroll } from '~/services/Hooks';
+import ChipCarousel from '../ChipCarousel/ChipCarousel';
+import HtmlCssJs from './components/HtmlCssJS/HtmlCssJs';
+import HtmlHowItWorks from './components/HtmlHowItWorks/HtmlHowItWorks';
+import HtmlFakeDiploma from './components/HtmlFakeDiploma/HtmlFakeDiploma';
+
+import styles from './HtmlLanding.module.scss';
+
+const HtmlLanding = ({ isLoggedIn }: ConnectedProps<typeof connector>) => {
+  const router = useRouter();
+  const [showHtmlCssJs, setShowHtmlCssJs] = useState(false);
+  const chipRows = [
+    ['<html>', '<div>', '<form>', '<head>', '<span>', '<article>', '<video>', '<button>', '<title>', '<main>', '<label>', '<summary>'],
+    ['<aside>', '<pre>', '<code>', '<em>', '<br>', '<body>', '<header>', '<section>', '<p>', '<nav>', '<tbody>', '<progress>', '<h1>'],
+    ['<blockquote>', '<ol>', '<footer>', '<audio>', '<img>', '<picture>', '<h2>', '<canvas>', '<figure>', '<hr>', '<ul>', '<select>'],
+    ['<a>', '<time>', '<h3>', '<track>', '<iframe>', '<svg>', '<script>', '<link>', '<table>', '<input>', '<textarea>', '<details>'],
+  ];
+
+  const { ref, inView } = useInView({
+    threshold: 1,
+    triggerOnce: true,
+  });
+
+  const startTutorial = () => {
+    // Temporary redirect to the lessons page,
+    // until we finish implementing the HTML Tutorial page.
+    router.push('/lectii');
+  };
+
+  const navigateToFirstSection = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    document.querySelector('#what-is-html').scrollIntoView();
+  };
+
+  const navigateToLogin = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    document.querySelector('#auth').scrollIntoView();
+  };
+
+  useEffect(() => {
+    if (inView) {
+      setShowHtmlCssJs(true);
+    }
+  }, [inView]);
+
+  withSmoothScroll();
+
+  return (
+    <>
+      <Header theme="black" withNavMenu />
+      <main data-certification-page className={styles.HtmlLanding}>
+        <div className={`${styles.hero} d-flex align-items-center justify-content-center bg-black text-white`}>
+          <h1 className="text-center mb-0"> Învață HTML de la zero </h1>
+          <p>
+            printr-un curs online, focusat pe
+            {' '}
+            <span className={styles['text-chip']}>
+              practică și feedback
+            </span>
+            {' '}
+            de la developeri cu experiență
+          </p>
+          <div className={`${styles['hero-controls']} d-flex align-items-center justify-content-between`}>
+            <a onClick={navigateToFirstSection} href="#what-is-html" className="btn btn--light no-underline">
+              Află mai multe
+            </a>
+
+            {isLoggedIn ? (
+              <Button variant="blue" onClick={startTutorial}>
+                Începe acum
+              </Button>
+            ) : (
+              <a onClick={navigateToLogin} href="#auth" className="btn btn--blue no-underline">
+                Începe acum
+              </a>
+            )}
+          </div>
+          <div className={styles['hero-video']} />
+        </div>
+
+        <div id="what-is-html">
+          <div className={styles.section}>
+            <h2 className={styles['section-heading']}>
+              Ce este HTML-ul?
+            </h2>
+
+            <p className={styles['section-text']}>
+              Este punctul de start în călătoria fiecărui
+              {' '}
+              <a href="/intro/ce-este-frontend-ul">FrontEnd developer</a>
+              .
+            </p>
+            <p className={styles['section-text']}>
+              Alături de
+              {' '}
+              <span className="text-bold"> CSS </span>
+              {' '}
+              și
+              {' '}
+              <span className="text-bold"> JavaScript </span>
+              {' '}
+              este unul din cele 3 limbaje pe care
+              trebuie să le înveți pentru a construi site-uri și aplicații web.
+            </p>
+          </div>
+          <div ref={ref} />
+          <div className={styles['HtmlCssJs-wrapper']}>
+            {showHtmlCssJs && <HtmlCssJs />}
+          </div>
+        </div>
+
+        <div>
+          <section className={styles.section}>
+            <h2 className={styles['section-heading']}>
+              Ce vei învăța?
+            </h2>
+            <p className={styles['section-text']}>
+              Cursul acesta e destinat persoanelor cu zero (sau foarte puțină)
+              experiență în FrontEnd, deci vom începe de la lucrurile de bază
+              și apoi continua cu multă practică.
+            </p>
+            <List className={`${styles['learn-list']} ${styles['section-text']}`} variant="checkmark">
+              <li className="mt-8">
+                Ce e FrontEnd development-ul?
+              </li>
+              <li className="mt-4">
+                Care e rolul HTML-ului în cadrul ramurii de FrontEnd?
+              </li>
+              <li className="mt-4">
+                Cum să folosești
+                {' '}
+                <a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer">
+                  VSCode
+                </a>
+                {' '}
+                ca și editor de cod
+              </li>
+              <li className="mt-4">
+                Care sunt și cum să folosești cele mai importante elemente HTML
+              </li>
+              <li className="mt-4">
+                Bune practici în zona de scriere a codului, accesibilitate și perfomanță
+              </li>
+            </List>
+
+          </section>
+          <ChipCarousel className="my-10" rows={chipRows} />
+        </div>
+
+        <div>
+          <div className={styles.section}>
+            <h2 className={styles['section-heading']}>
+              Cum funcționează cursul?
+            </h2>
+          </div>
+          <HtmlHowItWorks className={styles.HtmlHowItWorks} />
+        </div>
+
+        <div>
+          <div className={styles.section}>
+            <h2 className={styles['section-heading']}>
+              Iar la final primești o certificare!
+            </h2>
+            <p className={styles['section-text']}>
+              În programare nu contează prea mult diplomele în sine, ci ce știi să faci.
+              De aceea, folosește această certificare ca o dovadă că ai reușit să scrii cod
+              real, gata de producție, validat de developeri cu experiență!
+            </p>
+          </div>
+          <div className={styles.section}>
+            <HtmlFakeDiploma />
+          </div>
+        </div>
+
+        <div id="auth">
+          <div className={styles.section}>
+            <h2 className={styles['section-heading']}>
+              Gata să începem?
+            </h2>
+            {isLoggedIn && (
+              <p className={styles['section-text']}>
+                Ești deja autentificat, deci apasă pe butonul de mai jos și hai să trecem la treabă!
+              </p>
+            )}
+          </div>
+          {isLoggedIn ? (
+            <div className="text-center text-2xl">
+              <Button onClick={startTutorial} variant="blue">
+                Începe acum
+              </Button>
+            </div>
+          ) : (
+            <div className={styles['login-wrapper']}>
+              <Login
+                mode="register"
+                className={styles.login}
+                onSuccess={startTutorial}
+              />
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
+
+function mapStateToProps(state: RootState) {
+  return {
+    isLoggedIn: !!state.user.info,
+  };
+}
+
+const connector = connect(mapStateToProps);
+
+export default connector(HtmlLanding);
---
+++ client/components/HtmlLanding/components/HtmlHowItWorks/HtmlHowItWorks.tsx
@@ -0,0 +1,269 @@
+import React from 'react';
+import Highlight from '~/components/Highlight/Highlight';
+import { ProgressBar } from '~/components/progress';
+import StickyScrollExplainer from '~/components/StickyScrollExplainer/StickyScrollExplainer';
+import { getTextSection } from '~/services/Utils';
+import ModifySolution from '../ModifySolution/ModifySolution';
+import ReadTheLesson from '../ReadTheLesson/ReadTheLesson';
+import ReceiveFeedback from '../ReceiveFeedback/ReceiveFeedback';
+import SolutionApproved from '../SolutionApproved/SolutionApproved';
+
+import styles from './HtmlHowItWorks.module.scss';
+
+const HtmlHowItWorks = ({ className }: { className?: string}) => {
+  return (
+    <StickyScrollExplainer
+      breakpoint={800}
+      Explainer={Explainer}
+      Animation={FullAnimation}
+      className={className}
+    />
+  );
+};
+
+const Explainer = () => {
+  const explanations = [{
+    title: 'Citești lecția',
+    paragraphs: [
+      <> Fiecare concept e explicat printr-o lecție text dar și un tutorial video. </>,
+    ],
+  }, {
+    title: 'Rezolvi exercițiile',
+    paragraphs: [
+      <>
+        La finalul fiecărei lecții vei găsi unul sau mai multe exerciții,
+        pe care trebuie să le
+        {' '}
+        <span className="text-bold"> rezolvi </span>
+        {' '}
+        iar apoi să ni le trimiți pentru
+        {' '}
+        <span className="text-bold">feedback</span>
+        .
+      </>,
+    ],
+  }, {
+    title: 'Îți evaluăm soluția',
+    paragraphs: [
+      <>
+        Ne uităm pe fiecare exercițiu rezolvat
+        să vedem dacă e corect și respectă bunele practici
+        de programare.
+      </>,
+    ],
+  }, {
+    title: 'Îți trimitem feedback',
+    paragraphs: [
+      <>
+        Dacă exercitiul nu e corect, îți trimitem
+        feedback de îmbunătățire direct în platformă.
+      </>,
+    ],
+  }, {
+    title: 'Modifici soluția',
+    paragraphs: [
+      <>
+        Pe baza feedback-ului primit vei modifica soluția,
+        iar apoi o trimiți încă o dată către evaluare.
+      </>,
+    ],
+  }, {
+    title: 'O evaluăm din nou',
+    paragraphs: [
+      <>
+        Ne uitam iar peste această nouă soluție, să confirmăm
+        dacă schimbările tale sunt corecte.
+      </>,
+    ],
+  }, {
+    title: 'Îți acceptăm soluția!',
+    paragraphs: [],
+  }];
+
+  return (
+    <>
+      {explanations.map((explanation, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <section key={index} className={styles.ExplainerWrapper}>
+          <div className={styles.Explainer}>
+            <h3 className={styles['section-heading']}>
+              {explanation.title}
+            </h3>
+            {explanation.paragraphs.map((p, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <p key={index} className={styles['section-text']}>
+                {p}
+              </p>
+            ))}
+          </div>
+        </section>
+      ))}
+    </>
+  );
+};
+
+const FullAnimation = ({ percentage }: { percentage: number }) => {
+  const code = `<!DOCTYPE html>
+<html lang="ro">
+<head>
+  <title>Exercitiu HTML</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <form>
+    <input type="text" name="name" required>
+    <input type="email" name="email" required> 
+  </form>
+</body>
+</html>`;
+
+  const finalCode = `<!DOCTYPE html>
+<html lang="ro">
+<head>
+  <title>Exercitiu HTML</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <form>
+    <label>
+      <span> Nume </span>
+      <input type="text" name="name" required>
+    </label>
+    <label>
+      <span> Email </span>
+      <input type="email" name="email" required>
+    </label>
+  </form>
+</body>
+</html>`;
+
+  const timeline = React.useMemo(() => [
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <ReadTheLesson
+          className={styles.ReadTheLesson}
+          percentage={withDelay(timelineItemPercentage, 15, 25)}
+        />
+      </div>
+    ),
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <SolveTheExercise
+          percentage={withDelay(timelineItemPercentage, 25)}
+        />
+      </div>
+    ),
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <ProgressBar
+          className={`w-100 ${styles.ProgressBar}`}
+          count={100}
+          items={[
+            { count: timelineItemPercentage, color: '#9580ff' },
+          ]}
+          direction="horizontal"
+        />
+      </div>
+    ),
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <ReceiveFeedback
+          code={code}
+          feedbackSections={[
+            '<input type="text" name="name" required>',
+            '<input type="email" name="email" required>',
+          ]}
+          language="html"
+          percentage={withDelay(timelineItemPercentage)}
+          feedbackText="Fiecare element de tip <input> trebuie să aibă un <label> asociat."
+        />
+      </div>
+    ),
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <ModifySolution
+          startingCode={code}
+          finalCode={finalCode}
+          language="html"
+          className={styles.Highlight}
+          percentage={withDelay(timelineItemPercentage)}
+        />
+      </div>
+    ),
+    ({ timelineItemPercentage }) => (
+      <div className={styles.Animation}>
+        <ProgressBar
+          className={`w-100 ${styles.ProgressBar}`}
+          count={100}
+          items={[
+            { count: timelineItemPercentage, color: '#58af58' },
+          ]}
+          direction="horizontal"
+        />
+      </div>
+    ),
+    () => (
+      <div className={styles.Animation}>
+        <SolutionApproved />
+      </div>
+    ),
+  ], []);
+
+  const step = 100 / timeline.length;
+  const currentElementIndex = timeline.findIndex((_, index) => {
+    return (step * index) <= percentage && (step * (index + 1)) >= percentage;
+  });
+
+  const currentElementPercentage = (percentage - (step * currentElementIndex)) / step;
+  const Component = timeline[currentElementIndex];
+
+  return (
+    <Component timelineItemPercentage={currentElementPercentage * 100} />
+  );
+};
+
+const SolveTheExercise = ({ percentage }: { percentage: number }) => {
+  const code = `<!DOCTYPE html>
+<html lang="ro">
+<head>
+  <title>Exercitiu HTML</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <form>
+    <input type="text" name="name" required>
+    <input type="email" name="email" required>
+  </form>
+</body>
+</html>`;
+
+  const currentCode = getTextSection(code, percentage);
+
+  return (
+    <Highlight
+      language="html"
+      withCopy={false}
+      code={currentCode}
+      variant="minimalist"
+      textWrap="wrap"
+      className={styles.Highlight}
+    />
+  );
+};
+
+export default HtmlHowItWorks;
+
+function withDelay(realPercentage: number, endDelay = 25, beginningDelay = 0) {
+  if (realPercentage <= beginningDelay) {
+    return 0;
+  }
+
+  if (realPercentage >= 100 - endDelay) {
+    return 100;
+  }
+
+  return ((realPercentage - beginningDelay) / (100 - endDelay - beginningDelay)) * 100;
+}

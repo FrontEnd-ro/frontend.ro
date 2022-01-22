d6dddb3b844d1924cb899492072581af2507c8a7
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-22 21:38:54 +0200

    Highlight component doesn't re-render if 'code' prop changes (https://github.com/FrontEnd-ro/frontend.ro/issues/521)

--- client/components/Highlight/Highlight.tsx
+++ client/components/Highlight/Highlight.tsx
@@ -1,6 +1,7 @@
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useRef } from 'react';
+import { HLJSApi } from 'highlight.js';
 import { useClipboard } from '~/services/Hooks';
 import Button from '~/components/Button';
 
@@ -15,16 +16,47 @@ interface Props {
 }
 
 export default function Highlight({ language, code, className }: Props) {
-  const [preRef, btnRef] = [useRef(null), useRef(null)];
+  const highlightModule = useRef<HLJSApi>(null);
+  const preRef = useRef<HTMLPreElement>(null);
+  const btnRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     import('~/services/highlight').then((module) => {
-      const highlight = module.default;
-      highlight.highlightElement(preRef.current);
+      highlightModule.current = module.default;
+      highlightCode();
     });
   }, []);
 
+  useEffect(() => {
+    if (!highlightModule.current) {
+      // This should only happen once, until the above
+      // `useEffect` finishes to load the Highlight library.
+      return;
+    }
+
+    // After each highlight the <code> element is deleted
+    // and replaced with styled <span> elements. We have to
+    // re-create it.
+    const codeEl = document.createElement('code');
+
+    // Using `document.createTextNode` to correctly escape HTML code
+    codeEl.appendChild(document.createTextNode(code));
+    preRef.current.innerHTML = null;
+    preRef.current.appendChild(codeEl);
+
+    highlightCode();
+  }, [code]);
+
   useClipboard(btnRef);
+
+  const highlightCode = () => {
+    if (!preRef.current) {
+      console.warn('Highlight.highlightCode: expected `preRef.current` to be populated.');
+      return;
+    }
+
+    highlightModule.current.highlightElement(preRef.current);
+  }
 
   return (
     <div className={`${styles.highlight} ${className}`}>

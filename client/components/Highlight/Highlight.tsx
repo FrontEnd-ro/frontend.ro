b1d1f42b2c544086c44a87147371a272c4799985
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-23 14:43:20 +0200

    Extend <Highlight> component to allow showing/hiding the copy button (https://github.com/FrontEnd-ro/frontend.ro/issues/527)

--- client/components/Highlight/Highlight.tsx
+++ client/components/Highlight/Highlight.tsx
@@ -13,12 +13,12 @@ interface Props {
   code: string,
   language: Language,
   className?: string;
+  withCopy?: boolean;
 }
 
-export default function Highlight({ language, code, className }: Props) {
+export default function Highlight({ language, code, withCopy = true, className }: Props) {
   const highlightModule = useRef<HLJSApi>(null);
   const preRef = useRef<HTMLPreElement>(null);
-  const btnRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     import('~/services/highlight').then((module) => {
@@ -47,8 +47,6 @@ export default function Highlight({ language, code, className }: Props) {
     highlightCode();
   }, [code]);
 
-  useClipboard(btnRef);
-
   const highlightCode = () => {
     if (!preRef.current) {
       console.warn('Highlight.highlightCode: expected `preRef.current` to be populated.');
@@ -65,10 +63,20 @@ export default function Highlight({ language, code, className }: Props) {
           {code}
         </code>
       </pre>
-      <Button data-clipboard-text={code} ref={btnRef}>
-        <FontAwesomeIcon className="text-silver" icon={faCopy} width="32" />
-        <span className="d-block"> Copy </span>
-      </Button>
+      {withCopy && <CopyButton code={code} />}
     </div>
   );
+}
+
+const CopyButton = ({ code }: { code: string }) => {
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useClipboard(btnRef);
+
+  return (
+    <Button data-clipboard-text={code} ref={btnRef}>
+      <FontAwesomeIcon className="text-silver" icon={faCopy} width="32" />
+      <span className="d-block"> Copy </span>
+    </Button>
+  )
 }

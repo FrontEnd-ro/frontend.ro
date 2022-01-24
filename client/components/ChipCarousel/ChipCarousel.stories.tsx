5ef37d6a30c8e5b5ce635489d54183c4abf9536c
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-24 14:12:15 +0200

    Create a <ChipCarousel> UI component that animations certain code chips (https://github.com/FrontEnd-ro/frontend.ro/issues/530)

---
+++ client/components/ChipCarousel/ChipCarousel.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ChipCarouselComponent from './ChipCarousel';
+
+export default {
+  title: 'Generic/Chip Carousel',
+  component: ChipCarouselComponent,
+} as ComponentMeta<typeof ChipCarouselComponent>;
+
+const Template: ComponentStory<typeof ChipCarouselComponent> = (args) => (
+  <ChipCarouselComponent {...args} />
+);
+
+export const ChipCarousel = Template.bind({});
+ChipCarousel.args = {
+  rows: [
+    ['<html>', '<div>', '<form>', '<head>', '<span>', '<article>', '<video>', '<button>', '<title>', '<main>', '<label>', '<summary>'],
+    ['<aside>', '<pre>', '<code>', '<em>', '<br>', '<body>', '<header>', '<section>', '<p>', '<nav>', '<tbody>', '<progress>', '<h1>'],
+    ['<blockquote>', '<ol>', '<footer>', '<audio>', '<img>', '<picture>', '<h2>', '<canvas>', '<figure>', '<hr>', '<ul>', '<select>'],
+    ['<a>', '<time>', '<h3>', '<track>', '<iframe>', '<svg>', '<script>', '<link>', '<table>', '<input>', '<textarea>', '<details>']
+  ]  
+};
---
+++ client/components/ChipCarousel/ChipCarousel.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './ChipCarousel.module.scss';
+
+interface Props {
+  rows: (string[])[];
+
+  // Repeat each row horizontally, so there are enough
+  // elements on each of them for the animation.
+  repeatCount?: number;
+  className?: string;
+}
+
+const ChipCarousel = ({ rows, repeatCount = 3, className = "" }: Props) => {
+  const repeatedRows = rows.map((row) => {
+    let result = [];
+    for (let i = 0; i < repeatCount; i++) {
+      result = [...result, ...row];
+    }
+
+    return result;
+  })
+
+  return (
+    <div className={`${styles.ChipCarousel} ${className} w-100`}>
+      {repeatedRows.map((row, rowIndex) => (
+        <div className={`${styles.row} d-flex justify-content-center`} key={rowIndex}>
+          {row.map((chip, chipIndex) => (
+            <span className={styles.chip} key={chipIndex}> {chip} </span>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ChipCarousel;

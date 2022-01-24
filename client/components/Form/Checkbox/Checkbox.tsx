4bda2943a66d65388fca920ae1266592113d66fb
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-24 18:20:37 +0200

    Extend <Checkbox> component with 'black' variant (https://github.com/FrontEnd-ro/frontend.ro/issues/531)

--- client/components/Form/Checkbox/Checkbox.tsx
+++ client/components/Form/Checkbox/Checkbox.tsx
@@ -4,16 +4,18 @@ import styles from './Checkbox.module.scss';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
+  variant?: 'success' | 'black';
 }
 
 export default function Checkbox({
   children,
   className = '',
+  variant = 'success',
   ...props
 }: PropsWithChildren<Props>) {
   return (
     <label className={`${styles['checkbox-wrapper']} ${className}`}>
-      <span className={styles.checkbox}>
+      <span className={`${styles.checkbox} ${styles[variant]}`}>
         <input className="absolute" type="checkbox" {...props} />
         <span className={`${styles['control-indicator']} d-inline-block`} />
       </span>

9ea6a562ddbe0acdf68e1e99eaa6b39f7bfb2fe1
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-28 11:27:32 +0200

    Create the <SolutionApproved> component to be used for the new HTML Module landing page (https://github.com/FrontEnd-ro/frontend.ro/issues/538)

---
+++ client/components/HtmlLanding/components/SolutionApproved/SolutionApproved.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import ConfettiGenerator from 'confetti-js';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './SolutionApproved.module.scss';
+
+const SolutionApproved = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const confetti = new ConfettiGenerator({
+      target: canvasRef.current,
+    });
+    confetti.render();
+  }, []);
+  return (
+    <>
+      <div className={styles.SolutionApproved}>
+        <FontAwesomeIcon icon={faCheck} width={32} className="mr-2" />
+        Soluția e corectă!
+      </div>
+      <canvas className="absolute w-100 h-100" ref={canvasRef} />
+    </>
+  );
+};
+
+export default SolutionApproved;

8ed13a8f63be681d6250941511d1c12aa2c2d27d
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-01-28 11:46:03 +0200

    Create a generic UI component allowing stick-animation explainers (https://github.com/FrontEnd-ro/frontend.ro/issues/518)

---
+++ client/components/StickyScrollExplainer/StickyScrollExplainer.stories.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import StickyScrollExplainerComponent from './StickyScrollExplainer';
+
+export default {
+  title: 'Generic/Sticky Scroll Explainer',
+  component: StickyScrollExplainerComponent,
+} as ComponentMeta<typeof StickyScrollExplainerComponent>;
+
+const Template: ComponentStory<typeof StickyScrollExplainerComponent> = (args) => (
+  <div style={{
+    margin: '2em auto',
+    width: '90%',
+    height: '500px',
+    maxWidth: '1200px',
+  }}
+  >
+    <h1> Content to simulate a long page </h1>
+    <ul>
+      <li> Australia </li>
+      <li> Canada </li>
+      <li> Saudi Arabia </li>
+      <li> United States </li>
+      <li> India </li>
+      <li> Russia </li>
+      <li> South Africa </li>
+      <li> Turkey </li>
+      <li> Argentina </li>
+      <li> Brazil </li>
+      <li> Mexico </li>
+      <li> France </li>
+      <li> Germany </li>
+      <li> Italy </li>
+      <li> United Kingdom </li>
+      <li> China </li>
+      <li> Indonesia </li>
+      <li> Japan </li>
+      <li> South Korea </li>
+    </ul>
+    <StickyScrollExplainerComponent {...args} />
+    <h1> Content to simulate a long page </h1>
+    <ul>
+      <li> Australia </li>
+      <li> Canada </li>
+      <li> Saudi Arabia </li>
+      <li> United States </li>
+      <li> India </li>
+      <li> Russia </li>
+      <li> South Africa </li>
+      <li> Turkey </li>
+      <li> Argentina </li>
+      <li> Brazil </li>
+      <li> Mexico </li>
+      <li> France </li>
+      <li> Germany </li>
+      <li> Italy </li>
+      <li> United Kingdom </li>
+      <li> China </li>
+      <li> Indonesia </li>
+      <li> Japan </li>
+      <li> South Korea </li>
+    </ul>
+  </div>
+);
+
+const ExplanationExample = () => (
+  <div style={{ border: '1px solid #ccc', padding: '10em 0' }}>
+    <p className="my-10">Lorem Ipsum is simply dummy text</p>
+    <p className="my-10">of the printing and typesetting industry.</p>
+    <p className="my-10">Lorem Ipsum has been the industry's</p>
+    <p className="my-10">standard dummy text ever since the 1500s,</p>
+    <p className="my-10">when an unknown printer took a galley of</p>
+    <p className="my-10">type and scrambled it to make a type</p>
+    <p className="my-10">specimen book. It has survived not only</p>
+    <p className="my-10">five centuries, but also the leap into</p>
+    <p className="my-10">electronic typesetting, remaining essentially</p>
+    <p className="my-10">unchanged. It was popularised in the</p>
+    <p className="my-10">1960s with the release of Letraset sheets</p>
+    <p className="my-10">containing Lorem Ipsum passages, and more</p>
+    <p className="my-10">recently with desktop publishing software</p>
+    <p className="my-10">like Aldus PageMaker including versions of</p>
+    <p className="my-10">Lorem Ipsum.</p>
+  </div>
+);
+
+const AnimationExample = ({ percentage }: { percentage: number }) => (
+  <div style={{ background: '#fff' }}>
+    <h1>
+      {' '}
+      Loading:
+      {' '}
+      {`${percentage}%`}
+    </h1>
+  </div>
+);
+
+export const StickyScrollExplainer = Template.bind({});
+StickyScrollExplainer.args = {
+  Explainer: ExplanationExample,
+  Animation: AnimationExample,
+  breakpoint: 800,
+};
---
+++ client/components/StickyScrollExplainer/StickyScrollExplainer.tsx
@@ -0,0 +1,121 @@
+import { noop } from 'lodash';
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './StickyScrollExplainer.module.scss';
+
+interface Props {
+  // Left-side component that explains the animations
+  // on the right.
+  Explainer: () => JSX.Element;
+
+  // Right-side animation component. It gets a percentage as prop,
+  // which changes whenever the scroll changes.
+  Animation: ({ percentage }: { percentage: number }) => JSX.Element;
+
+  // Breakpoint where to switch from `row` to `column` layout.
+  breakpoint?: number;
+  className?: string;
+}
+
+const StickyScrollExplainer = ({
+  Explainer,
+  Animation,
+  breakpoint = -1,
+  className = '',
+}: Props) => {
+  const [isColumn, setIsColumn] = useState(false);
+  const [percentage, setPercentage] = useState(0);
+
+  const rootRef = useRef<HTMLElement>(null);
+
+  // How much of the total height has it been scrolled
+  const computePercentage = () => {
+    // I think this have have a "delay", half of it's height!
+    const { offsetTop, offsetHeight } = rootRef.current;
+    let { scrollY, innerHeight } = window;
+
+    // This component will always be full screen (100vh)
+    // We want to start "counting" the percentage
+    // only when this component is halfway into view.
+    // Since this component will be full-screen,
+    // we subtract from scrollY half of the window height
+    scrollY -= innerHeight / 2;
+
+    const scrollOver = scrollY + innerHeight - offsetTop;
+
+    if (scrollOver / offsetHeight > 1) {
+      return 100;
+    }
+
+    if (scrollOver / offsetHeight < 0) {
+      return 0;
+    }
+
+    return (scrollOver / offsetHeight) * 100;
+  };
+
+  useEffect(() => {
+    setPercentage(computePercentage());
+
+    function onWindowScroll() {
+      setPercentage(computePercentage());
+    }
+
+    window.addEventListener('scroll', onWindowScroll);
+    return () => {
+      window.removeEventListener('scroll', onWindowScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (breakpoint === -1) {
+      // Ignore
+      return noop;
+    }
+
+    /**
+     * We want this component to be totally reusable, maybe even
+     * extract into a separate UI Library at some point. This
+     * means that CSS Modules do not work. Instead, Styled Components
+     * would be a great choice.
+     *
+     * But, we don't want to add them in our project yet,
+     * so going with this compromise aproach - using JS
+     * to listen to the window resize event.
+     */
+    const onWindowResize = () => {
+      if (window.innerWidth <= breakpoint && !isColumn) {
+        setIsColumn(true);
+      }
+
+      if (window.innerWidth > breakpoint && isColumn) {
+        setIsColumn(false);
+      }
+    };
+
+    onWindowResize();
+    window.addEventListener('resize', onWindowResize);
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+    };
+  }, [isColumn, breakpoint]);
+
+  return (
+    <section
+      ref={rootRef}
+      className={`
+        ${styles.StickyScrollExplainer}
+        ${isColumn ? styles['StickyScrollExplainer-column'] : styles['StickyScrollExplainer-row']}
+        ${className}
+      `}
+    >
+      <div className={styles.explainer}>
+        <Explainer />
+      </div>
+      <div className={styles.animation}>
+        <Animation percentage={percentage} />
+      </div>
+    </section>
+  );
+};
+
+export default StickyScrollExplainer;

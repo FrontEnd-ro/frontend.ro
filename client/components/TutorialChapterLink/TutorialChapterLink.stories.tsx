f2a73f49ca98705c232f2154562278236db34233
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-02-11 17:33:02 +0200

    Create a new <TutorialStepLink> component (https://github.com/FrontEnd-ro/frontend.ro/issues/566)

---
+++ client/components/TutorialChapterLink/TutorialChapterLink.stories.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import TutorialChapterLinkComponent from './TutorialChapterLink';
+import { Chapter } from '../TableOfContents';
+
+export default {
+  title: 'Tutoria/TutorialChapterLink',
+  component: TutorialChapterLinkComponent,
+} as ComponentMeta<typeof TutorialChapterLinkComponent>;
+
+const Template: ComponentStory<typeof TutorialChapterLinkComponent> = (args) => (
+  <TutorialChapterLinkComponent {...args} />
+);
+
+const BASE_PROPS: {
+  href: string,
+  title: string,
+  chapters: Chapter[],
+  showChapters: boolean,
+} = {
+  href: '#',
+  title: 'HTML Lesson',
+
+  chapters: [{
+    id: 'ch-1',
+    href: '#',
+    title: 'Head element',
+  }, {
+    id: 'ch-2',
+    href: '#',
+    title: 'Body element',
+  }, {
+    id: 'ch-3',
+    href: '#',
+    title: 'Image elements',
+  }],
+  showChapters: false,
+};
+
+export const Initial = Template.bind({});
+Initial.args = {
+  ...BASE_PROPS,
+  completePercentage: 0,
+};
+
+export const InProgress = Template.bind({});
+InProgress.args = {
+  ...BASE_PROPS,
+  completePercentage: 33,
+};
+
+export const Done = Template.bind({});
+Done.args = {
+  ...BASE_PROPS,
+  completePercentage: 100,
+};
+
+export const WithChapters = Template.bind({});
+WithChapters.args = {
+  ...BASE_PROPS,
+  completePercentage: 33,
+  showChapters: true,
+};
---
+++ client/components/TutorialChapterLink/TutorialChapterLink.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ProgressDonut } from '~/components/progress';
+import TableOfContents, { Chapter } from '~/components/TableOfContents';
+
+import styles from './TutorialChapterLink.module.scss';
+
+interface Props {
+  href: string;
+  title: string;
+
+  chapters: Chapter[];
+  showChapters: boolean;
+
+  // A number between [0, 100]
+  completePercentage: number;
+}
+
+const TutorialChapterLink = ({
+  href,
+  title,
+  completePercentage,
+  chapters = [],
+  showChapters = false,
+}: Props) => {
+  const router = useRouter();
+
+  return (
+    <div className={`${styles.TutorialChapterLink} relative overflow-hidden rounded-md`}>
+      <Link href={href}>
+        <a className={`
+          ${styles.link}
+          ${router.pathname === href ? styles.active : ''}
+          d-flex
+          align-items-center
+          no-underline
+        `}
+        >
+          {(completePercentage > 0) && (
+            <ProgressIndicator completePercentage={completePercentage} />
+          )}
+          <span className={styles.title}>
+            {title}
+          </span>
+        </a>
+      </Link>
+      {showChapters && (
+        <div className={styles.chapters}>
+          <TableOfContents chapters={chapters} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const ProgressIndicator = ({
+  completePercentage,
+}: {
+  completePercentage: number
+}) => {
+  const donutItems = (completePercentage < 100)
+    ? [{
+      count: completePercentage,
+      color: 'var(--green)',
+    }, {
+      count: 100 - completePercentage,
+      color: 'var(--silver)',
+    }]
+    : [{ count: 100, color: 'var(--green) ' }];
+
+  return (
+    <div className={`${styles['progress-wrapper']} relative`}>
+      <ProgressDonut
+        size="2em"
+        count={100}
+        strokeWidth={3}
+        items={donutItems}
+      />
+      {completePercentage === 100 && (
+        <FontAwesomeIcon
+          width={14}
+          icon={faCheck}
+          className={`absolute text-green ${styles['check-icon']}`}
+        />
+      )}
+    </div>
+  );
+};
+
+export default TutorialChapterLink;

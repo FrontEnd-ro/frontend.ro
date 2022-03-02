fa8cdc6b5fe7b26f5c77a5214cc023d805823d0c
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-03-02 20:48:00 +0200

    Add a new GET by tutorial id to the tutorial router (https://github.com/FrontEnd-ro/frontend.ro/issues/620)

--- server/tidbit/tidbit.router.ts
+++ server/tidbit/tidbit.router.ts
@@ -1,17 +1,33 @@
-import express, { Request, Response } from 'express';
-import Tidbit from './tidbit.model';
-import { PublicMiddleware } from '../Middlewares';
-import { TidbitI } from '../../shared/types/tidbit.types';
+import express, { Request, Response } from "express";
+import Tidbit from "./tidbit.model";
+import { ServerError } from "../ServerUtils";
+import { PublicMiddleware } from "../Middlewares";
+import { TidbitI } from "../../shared/types/tidbit.types";
 
 const tidbitRouter = express.Router();
 
-tidbitRouter.get('/', [
+tidbitRouter.get("/", [
   PublicMiddleware,
-  async function getAllTutorials(req: Request, res: Response) {
+  async function getAllTidbits(req: Request, res: Response) {
     const tidbits: TidbitI[] = await Tidbit.find();
 
     res.json(tidbits);
-  }
+  },
+]);
+
+tidbitRouter.get("/:tidbitId", [
+  PublicMiddleware,
+  async function getTidbit(req: Request, res: Response) {
+    const { tidbitId } = req.params;
+    const tidbit: TidbitI = await Tidbit.findOne({ tidbitId });
+
+    if (tidbit === null) {
+      new ServerError(404, "Not found").send(res);
+      return;
+    }
+
+    res.json(tidbit);
+  },
 ]);
 
 export default tidbitRouter;

02e23c13e421544fe5403b2b1f5f0989e96c05ad
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-03-05 11:46:47 +0200

    Add a new API that gives the previous and next tidbits, given a certain tidbit (https://github.com/FrontEnd-ro/frontend.ro/issues/622)

--- server/tidbit/tidbit.router.ts
+++ server/tidbit/tidbit.router.ts
@@ -30,4 +30,26 @@ tidbitRouter.get("/:tidbitId", [
   },
 ]);
 
+tidbitRouter.get("/:currentTidbitId/sides", [
+  PublicMiddleware,
+  async function getPreviousAndNextTidbit(req: Request, res: Response) {
+    const { currentTidbitId } = req.params;
+
+    const tidbits: TidbitI[] = await Tidbit.find().sort("-createdDate");
+    const indexOf = tidbits.findIndex(
+      (tidbit) => tidbit.tidbitId === currentTidbitId
+    );
+
+    if (indexOf === -1) {
+      new ServerError(404, `Tidbit with id=${currentTidbitId} not found.`).send(res);
+      return;
+    }
+
+    res.json({
+      previous: tidbits[indexOf - 1] ?? null,
+      next: tidbits[indexOf +1] ?? null,
+    });
+  },
+]);
+
 export default tidbitRouter;

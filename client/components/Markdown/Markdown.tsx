67dffa4ed56bea48b0748c6df72da841be847d35
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-12-12 20:30:52 +0200

    Use highlight JS to automatically format markdown code (https://github.com/FrontEnd-ro/frontend.ro/issues/815)

--- client/components/Markdown/Markdown.tsx
+++ client/components/Markdown/Markdown.tsx
@@ -13,12 +13,34 @@ interface Props {
 }
 
 function Markdown({ markdownString, className = '', variant = 'default' }: Props) {
-  const markdownRef = useRef(null);
+  const markdownRef = useRef<HTMLDivElement>(null);
 
   // Whether or not we successfully lazy-loaded
   // the modules needed for this component (this happens)
   // in the `useEfffect` below
   const [didLoadModules, setDidLoadModules] = useState(undefined);
+
+  const highlightCode = async () => {
+    const SPAN = '[highlightCode]';
+    try {
+      const [highlightModule, styleModule] = await Promise.all([
+        import('~/services/highlight'),
+        import('../Highlight/Highlight.module.scss'),
+      ]);
+
+      if (markdownRef.current === null) {
+        return;
+      }
+
+      // Highlight code elements
+      markdownRef.current.classList.add(styleModule.default.highlight);
+      markdownRef.current.querySelectorAll('pre').forEach((el) => {
+        highlightModule.default.highlightElement(el);
+      });
+    } catch (err) {
+      console.error(`${SPAN} Failed to highlight markdown.`, err);
+    }
+  };
 
   useEffect(() => {
     Promise.all([
@@ -30,6 +52,8 @@ function Markdown({ markdownString, className = '', variant = 'default' }: Props
 
       setDidLoadModules(true);
       markdownRef.current.innerHTML = dompurify.sanitize(marked.parse(markdownString));
+
+      highlightCode();
     }).catch((err) => {
       setDidLoadModules(false);
       console.error('[Markdown.useEffect]', err);

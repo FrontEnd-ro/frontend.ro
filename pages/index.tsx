68193750092ff5223b6f3f78d00c23973b2adfe6
Alexandru Pavaloi <a.pavaloi@gmail.com>
2023-03-25 11:08:02 +0200

    Use Promise.all when fetching data for rendering the landing page (#896)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Response } from 'node-fetch';
 import LandingPage from '~/components/Landing';
 
 export default LandingPage;
@@ -9,41 +10,39 @@ export async function getServerSideProps(ctx) {
   const { default: fetch } = await import('node-fetch');
   const SPAN = '[pages/index.tsx, getServerSideProps]';
 
-  const resp = await fetch(`${process.env.ENDPOINT}/tidbits?field=title&field=textColor&field=backgroundColor&field=tidbitId&field=items[1].imageSrc`);
-  const tidbits = await resp.json();
-
   const pageProps = {
     props: {
-      tidbits,
+      tidbits: [],
       htmlTutorialProgress: null,
     },
   };
 
-  if (!token) {
-    return pageProps;
-  }
-
   try {
-    const { default: fetch } = await import('node-fetch');
+    const [tidbits, htmlTutorialProgress] = await Promise.all([
+      fetch(`${process.env.ENDPOINT}/tidbits?field=title&field=textColor&field=backgroundColor&field=tidbitId&field=items[1].imageSrc`)
+        .then(parseJSONOrThrowIfNot200),
+      !token
+        ? Promise.resolve(null)
+        : fetch(`${process.env.ENDPOINT}/tutorials/html/progress`, {
+          headers: {
+            cookie: `token=${token}`,
+          },
+        }).then(parseJSONOrThrowIfNot200),
+    ]);
 
-    // Accessing the DB code directly fails (cannot find module 'fs').
-    // It happened after adding the `node-config` package. Thus, to fix
-    // it we refactored this code to use `fetch`.
-    const resp = await fetch(`${process.env.ENDPOINT}/tutorials/html/progress`, {
-      headers: {
-        cookie: `token=${token}`,
-      },
-    });
-
-    if (!resp.ok) {
-      return pageProps;
-    }
-
-    const tutorialProgress = await resp.json();
-    pageProps.props.htmlTutorialProgress = tutorialProgress;
+    pageProps.props.tidbits = tidbits;
+    pageProps.props.htmlTutorialProgress = htmlTutorialProgress;
   } catch (err) {
     console.error(`${SPAN}`, err);
   }
 
   return pageProps;
+}
+
+function parseJSONOrThrowIfNot200(resp: Response) {
+  if (!resp.ok) {
+    throw new Error(`Unexpected response status: ${resp.status}`);
+  }
+
+  return resp.json();
 }

f47ac191af25f7a51938d4d60ecd9bcd751b5295
Alexandru Pavaloi <a.pavaloi@gmail.com>
2023-03-30 16:16:57 +0300

    Covert the 'despre-noi' article to MDX (#917)

--- client/curriculum/intro/despre-noi.tsx
+++ client/curriculum/intro/despre-noi.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { useMemo } from 'react';
+import * as runtime from 'react/jsx-runtime'
 import {
   LessonCover, LessonHeading, LessonFigure, LessonQuote,
 } from '~/components/lessons';
 import {
   FacebookButton, LinkedInButton, WhatsAppButton, CopyLinkButton,
 } from '~/components/SocialMediaButtons';
-import { useCurrentUrl } from '~/services/Hooks';
-import { getLessonById } from '~/services/DataModel';
 import coverSvg from './despre-noi-cover.svg';
 import FormattedText from '~/components/FormattedText';
 import List from '~/components/List';
+import { runSync } from '@mdx-js/mdx';
 import OptionsDrawer from '~/components/OptionsDrawer/OptionsDrawer';
-import { GITHUB_URL } from '~/services/Constants';
+import { MDXProvider, useMDXComponents } from '@mdx-js/react';
 
-export default function AboutUsContent() {
-  const urlToShare = useCurrentUrl();
-  const lessonInfo = getLessonById('despre-noi');
+
+const MDX_COMPONENTS = {
+  List,
+  LessonHeading,
+  LessonQuote,
+  OptionsDrawer,
+  CopyLinkButton,
+  FacebookButton,
+  LinkedInButton,
+  WhatsAppButton,
+  LessonFigure,
+  FormattedText
+}
+
+export default function AboutUsContent({ mdxContent }: { mdxContent: string }) {
+  const { default: Content } = useMemo(() => runSync(mdxContent, {
+    ...runtime,
+    useMDXComponents,
+  }), [mdxContent]);
 
   return (
     <>
@@ -28,372 +43,9 @@ export default function AboutUsContent() {
         />
 
       </LessonCover>
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[0].id}>
-          {lessonInfo.chapters[0].title}
-        </LessonHeading>
-        <p>
-          FrontEnd.ro este o comunitate de oameni ce învață FrontEnd de la zero.
-        </p>
-        <p>
-          Comunitatea gravitează în jurul unei serii de lecții și exerciții
-          gratuite ce constituie - în opinia noastră - fundamentele unei cariere moderne
-          în FrontEnd Development. Deși abia la
-          {' '}
-          <span className="line-through">prima</span>
-          {' '}
-          a doua iterație, acestea vor fi
-          re-scrise, re-re-scrise și re-re-re-scrise în funcție de feedback-ul vostru
-          până când vor ajunge una din cele mai calitative resurse de acest fel din România.
-        </p>
-      </section>
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[1].id}>
-          {lessonInfo.chapters[1].title}
-        </LessonHeading>
-        <p>
-          Noi suntem convinși că:
-        </p>
-        <LessonQuote variant="centered">
-          Nu contează la câte tutoriale te uiți, ci cât de mult codezi
-        </LessonQuote>
-        <p>
-          Însă când înveți singur e puțin mai greu să-ți dai seama dacă rezolvarea
-          găsită de tine este cea mai bună. Așa că ne vei trimite aici rezolvările
-          la exerciții, iar noi - developeri cu experiență din comunitate -  îți
-          vom da feedback pentru fiecare.
-        </p>
-      </section>
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[2].id}>
-          {lessonInfo.chapters[2].title}
-        </LessonHeading>
-        <p>
-          De la început ne-am dorit ca FrontEnd.ro să fie un
-          {' '}
-          <strong>
-            efort de comunitate,
-            pentru comunitate
-          </strong>
-          . Lecțiile, exercițiile, acest site cât și feedback-ul
-          pe care voi îl veți primi reprezintă o contribuție voluntară a unor
-          developeri cu inimă mare din România.
-        </p>
-        <p>
-          Vrem să oferim șansa la cât
-          mai mulți oameni să învețe și să lucreze în acest domeniu, unul din cele mai
-          faine din lume, de aceea nu vom pune un preț pe această experiență.
-        </p>
-      </section>
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[3].id}>
-          {lessonInfo.chapters[3].title}
-        </LessonHeading>
-        <p>
-          Dacă folosești platforma să
-          {' '}
-          <strong>înveți FrontEnd</strong>
-          {' '}
-          și simți că te-a ajutat, atunci s-ar putea să fie de folos și prietenilor
-          tăi. Am aprecia mult un share, așa putem avea un impact cât mai mare:
-        </p>
-        <div className="d-flex justify-content-center my-5">
-          <OptionsDrawer trigger={{ text: 'Share', icon: faShare }}>
-            <OptionsDrawer.Element className="bg-white">
-              <CopyLinkButton text={urlToShare} />
-            </OptionsDrawer.Element>
-            <OptionsDrawer.Element className="bg-white">
-              <FacebookButton url={urlToShare} />
-            </OptionsDrawer.Element>
-            <OptionsDrawer.Element className="bg-white">
-              <LinkedInButton url={urlToShare} />
-            </OptionsDrawer.Element>
-            <OptionsDrawer.Element className="bg-white">
-              <WhatsAppButton url={urlToShare} />
-            </OptionsDrawer.Element>
-          </OptionsDrawer>
-        </div>
-        <p>
-          Iar după ce avansezi în skill-uri și capeți mai multă experiență, chiar ne-am bucura
-          dacă vei reveni aici, de data aceasta din postura de mentor și să oferi
-          înapoi comunității. 😄
-        </p>
-        <p>
-          Dacă
-          {' '}
-          <strong>ești FrontEnd Developer</strong>
-          , atunci ne poți ajuta prin oferirea de
-          feedback cursanților, crearea de exerciții și lecții sau dezvoltarea
-          acestei platforme al cărei cod este
-          {' '}
-          <a target="_blank" rel="noreferrer" href={GITHUB_URL}>open-source pe GitHub</a>
-          .
-          {/* Mai mult despre acest proces gasesti in capitolul
-                {' '}
-                <a href="/intro/implica-te">Cum ajut</a>
-                . */}
-        </p>
-        <p>
-          Iar dacă
-          {' '}
-          <strong>ești o firmă tech și-ți place ce încercăm să facem aici</strong>
-          ,
-          poate găsim un mod să colaborăm și să ne ajutăm reciproc și comunitatea
-          în același timp.
-          {/*
-            Aruncă un ochi
-            {' '}
-            <a href="/intro/implica-te">aici</a>
-            {' '}
-            pentru mai multe detalii.
-             */}
-        </p>
-      </section>
-      <div className="dots" />
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[4].id}>
-          {lessonInfo.chapters[4].title}
-        </LessonHeading>
-        <p>
-          FrontEnd-ul, de fapt programarea în general, este un domeniu în continuă
-          dezvoltare și schimbare.
-          În fiecare lună apar lucruri noi, iar în cațiva ani tool-urile pe care
-          noi le folosim pot fi complet diferite. Ceea ce face
-          lucrurile extrem de interesante, dar și puțin complicate
-          când vrei să înveți acest domeniu...
-        </p>
-
-        <p>
-          Totuși, fundamentele FrontEnd-ului nu se schimbă, și le vei folosi fie că
-          construiești un site de prezentare, un magazin online sau următorul
-          Instagram. De aceea, aici ne vom ocupa de fundamentele celor
-          3 limbaje de programare ce compun FrontEnd-ul:
-          {' '}
-          <strong>HTML</strong>
-          ,
-          {' '}
-          <strong>CSS</strong>
-          {' '}
-          și
-          {' '}
-          <strong>JavaScript</strong>
-          . Deocamdată avem lecții și exerciții doar pentru
-          primul, cel de HTML, însa după o rundă de feedback de la
-          comunitate vom continua și cu celelalte două.
-        </p>
-      </section>
-      <div className="dots" />
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[5].id}>
-          {lessonInfo.chapters[5].title}
-        </LessonHeading>
-        <p>
-          Focusul nostru este pe exerciții și feedback, acolo investim cel
-          mai mult timp. Există deja o mulțime de articole și tutoriale
-          extrem de bune pe internet, așa că am decis ca lecțiile să reprezinte
-          doar un
-          {' '}
-          <strong>scurt rezumat</strong>
-          ,  care la final va avea
-          {' '}
-          <strong>
-            link-uri către resurse
-            mai detaliate
-          </strong>
-          . Din această cauză, principala responsabilitate
-          la partea de învățare este a ta.
-          {' '}
-        </p>
-        <LessonQuote>
-          Ne așteptăm să fii motivat
-          și autodidact, să poți înțelege în mare conceptele de unul singur.
-        </LessonQuote>
-        <p>
-          Pentru a învăța cât mai mult, începe cu lecția 0 (cea pe care o citești acum)
-          și continuă pe rând cu fiecare lecție nouă. Citește rezumatul nostru, iar
-          apoi aruncă un ochi pe resursele suplimentare, înainte să te apuci
-          de exerciții.
-          {' '}
-        </p>
-        <p>
-          <strong>
-            Este foarte important să rezolvi toate exercițiile
-            de la fiecare lecție.
-          </strong>
-          {' '}
-          Ai răbdare și încredere în noi, nu am încărcat site-ul cu exerciții inutile.
-        </p>
-        <p>
-          Apoi, după ce ne trimiți soluția ta vom ajunge la
-          {' '}
-          <strong>partea de feedback</strong>
-          .
-          Fiind începător probabil vor fi undeva la 3-4-5 runde,
-          în care-ți vom explica de ce anumite lucruri nu sunt
-          bune și te vom ruga să le schimbi.
-        </p>
-        <p>
-          E extrem de important
-          să
-          {' '}
-          <strong>ai răbdare cu acest proces, și să nu fii prea dur cu tine</strong>
-          .
-          Bucură-te că ai pe cineva care-ți face Code Review și de la
-          care poți să înveți. Mulți dintre noi nu am avut șansa asta.
-          Și din nou:
-        </p>
-        <LessonQuote variant="centered">
-          Don't be too hard on yourself.
-          Don't take it personal.
-          Noi suntem aici să te ajutăm. 😄
-        </LessonQuote>
-      </section>
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[6].id}>
-          {lessonInfo.chapters[6].title}
-        </LessonHeading>
-        <p>
-          Unul dintre cele mai faine lucruri la FrontEnd development
-          este faptul că nu avem nevoie de prea multe lucruri înainte să începem.
-          În primul rând, un
-          {' '}
-          <strong>calculator</strong>
-          {' '}
-          și o
-          {' '}
-          <strong>conexiune la internet</strong>
-          .
-        </p>
-        <p> Apoi, specific pentru acest domeniu ai nevoie de:</p>
-        <List as="ol" variant="count">
-          <li>
-            <strong>un browser modern și cross-platform </strong>
-            (ce poate fi instalat pe
-            Windows, MacOS și Linux).
-            <p>
-              Noi recomandăm unul dintre
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://www.google.com/chrome/">
-                Google Chrome
-              </a>
-              ,
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://www.mozilla.org/en-US/firefox/new/">Mozilla Firefox</a>
-              {'  '}
-              sau
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://www.microsoft.com/en-us/edge">Microsoft Edge</a>
-              .
-              {' '}
-              Nu prea contează pe care din cele 3 îl instalezi, toate sunt bune.
-            </p>
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://code.visualstudio.com/download">Visual Studio Code</a>
-            : programul unde vom scrie efectiv codul.
-
-            <p>
-              {' '}
-              Acest editor este
-              extrem de popular printre developeri, așa că hai să-l folosim chiar de la
-              început pentru a ne familiariza cu el.
-              De asemenea, înainte să începi
-              {' '}
-              <a href="/html/despre-html">Lecția 1</a>
-              {' '}
-              te invităm să arunci un ochi pe acest
-              {' '}
-              <a href="/intro/vs-code">mini-tutorial despre VSCode</a>
-              .
-            </p>
-          </li>
-        </List>
-      </section>
-      <div className="dots" />
-      <section>
-        <LessonHeading as="h2" id={lessonInfo.chapters[7].id}>
-          {lessonInfo.chapters[7].title}
-        </LessonHeading>
-        <p>
-          Pe partea de resurse, vom adăuga la finalul fiecărei lecții link-uri de
-          unde poți învăța mai multe. Totuși, nu putem să nu menționăm două site-uri extrem de
-          utile și fără de care job-ul de FrontEnd developer ar fi mult mai frustrant:
-        </p>
-        <List as="ol" variant="count">
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://developer.mozilla.org/en-US/" className="text-bold">MDN - Mozilla Developer Network</a>
-            {' '}
-            este locul unde găsim cele mai calitative și detaliate documentații despre HTML,
-            CSS și JavaScript - cele 3 limbaje ce compun FrontEnd development-ul.
-            <p>
-              Domeniul e atât de complex încât e perfect normal să nu ținem minte totul.
-              E mult mai comun decât ai crede ca developeri cu experiență să uite
-              lucruri de sintaxă, adică ce bucată de cod trebuie să folosească pentru
-              a ajunge la un anumit rezultat.
-            </p>
-            <p>
-              De exemplu, poate am uitat cum schimbăm culoarea de fundal
-              a unui element din pagină, caz în care vom căuta
-              {' '}
-              <FormattedText as="span">background color MDN</FormattedText>
-              .
-              Recomandăm să pui “MDN” la final pentru ca pagina căutată
-              să apară printre primele rezultate.
-            </p>
-            <LessonFigure
-              withBorder
-              src="/images/lesson-0__google-search.png"
-              alt="Cautând `background-color MDN` pe Google"
-            />
-            <p>
-              And voilà! Am ajuns la documentație,
-              unde putem vedea toate opțiunile posibile și cum le putem folosi:
-            </p>
-            <LessonFigure
-              withBorder
-              src="/images/lesson-0__mdn.png"
-              alt="Documentația `background-color` pe MDN"
-            />
-          </li>
-          <li>
-            <a target="_blank" rel="noreferrer" href="https://stackoverflow.com/" className="text-bold">Stack Overflow</a>
-            {' '}
-            este o comunitate de developeri ce învață și codează împreună.
-            <p>
-              Mai exact, este cea mai populară platformă de întrebări și
-              răspunsuri de programare, folosită de mai toți developerii din lume.
-              Fără acest site ne-ar lua mult
-              mai mult timp să gasim soluții la diversele
-              buguri/probleme pe care le întâlnim când codăm.
-            </p>
-            <p>
-              {' '}
-              Iar dacă ești curios, poți arunca un ochi pe ultimele întrebări adăugate pentru
-              limbajele ce le vom învăța aici:
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://stackoverflow.com/questions/tagged/html">HTML</a>
-              ,
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://stackoverflow.com/questions/tagged/css">CSS</a>
-              {' '}
-              si
-              {' '}
-              <a target="_blank" rel="noreferrer" href="https://stackoverflow.com/questions/tagged/javascript">JavaScript</a>
-              .
-            </p>
-          </li>
-        </List>
-      </section>
-      {/* FIXME: uncomment this */}
-      {/* <div className="text-center my-5">
-          <a href="/" className="cta-button">
-            Să continuăm cu prima lecție
-            {' '}
-            <br />
-            {' '}
-            despre HTML
-          </a>
-        </div> */}
+      <MDXProvider components={MDX_COMPONENTS}>
+        <Content />
+      </MDXProvider>
     </>
   );
 }
--- pages/intro/[id].tsx
+++ pages/intro/[id].tsx
@@ -4,14 +4,17 @@ import { NotWroteYet } from '~/components/404';
 import NotFoundPage from '~/components/404/NotFound';
 import VSCodeContent from '~/curriculum/intro/VSCode';
 import AboutUsContent from '~/curriculum/intro/despre-noi';
+import { GITHUB_URL } from '~/services/Constants';
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { compile } from '@mdx-js/mdx';
 import { getLessonById, LessonDescription } from '~/services/DataModel';
+import appConfig from '~/../server/config/config';
 
 const LESSON_TO_COMPONENT = {
-  'despre-noi': <AboutUsContent />,
   'vs-code': <VSCodeContent />,
 };
 
-const IntroLesson = ({ lessonInfo }: { lessonInfo: LessonDescription | null }) => {
+const IntroLesson = ({ lessonInfo, mdxContent }: { lessonInfo: LessonDescription | null, mdxContent: string }) => {
   if (lessonInfo === null) {
     return <NotFoundPage />;
   }
@@ -29,7 +32,11 @@ const IntroLesson = ({ lessonInfo }: { lessonInfo: LessonDescription | null }) =
         shareImage={lessonInfo.ogImage}
       />
       <Lesson lessonInfo={lessonInfo}>
-        {LESSON_TO_COMPONENT[lessonInfo.id]}
+        {lessonInfo.id === 'despre-noi' ? (
+          <AboutUsContent mdxContent={mdxContent} />
+        ) : (
+          LESSON_TO_COMPONENT[lessonInfo.id]
+        )}
       </Lesson>
     </>
   );
@@ -38,19 +45,50 @@ const IntroLesson = ({ lessonInfo }: { lessonInfo: LessonDescription | null }) =
 // We tried migrating to staticPaths BUT that conflicts
 // with the user fetch inside `_app.tsx` which means that navigating
 // directly to lesson pages leads to the website thinking you're logged out.
-export function getServerSideProps({ res, params }) {
+export async function getServerSideProps({ req, res, params }) {
   const { id } = params;
   const lessonInfo = getLessonById(id);
+  let mdxContent = '';
 
   if (lessonInfo === null) {
     res.statusCode = 404;
   }
 
+  if (lessonInfo.id === 'despre-noi') {
+    const { default: mdxAsString } = await import('!raw-loader!../../client/curriculum/intro/despre-noi.mdx');
+
+    const MDX_SCOPE = {
+      GITHUB_URL,
+      headings:  lessonInfo.chapters,
+      icons: { faShare },
+      urlToShare: `${appConfig.APP.app_url}${req.url}`
+    }
+    mdxContent = String(await compile(appendMdxScope(mdxAsString as unknown as string, MDX_SCOPE), {
+      development: false,
+      outputFormat: 'function-body',
+      // This is what the next-mdx-remote also does,
+      // so let's put the same values (instead of `#`).
+      providerImportSource: '@mdx-js/react',
+    }));
+  }
+
   return {
     props: {
       lessonInfo,
+      mdxContent,
     },
   };
+}
+
+// Kinda hackish way to inject outside variables/scope into MDX Files
+// As you can see we're doing this by specifically exporting a `SCOPE` variable
+// from the MDX file. Looks a bit weird, but should work as long as]
+// 1. The scope can be serialized to JSON
+// 2. We don't have another variable in the file called SCOPE
+// TODO: once we add more and more articles with MDX content, let's
+// move this into a separate file
+function appendMdxScope(compiledMdx: string, scope: Record<string, any>) {
+  return `export const SCOPE = ${JSON.stringify(scope)};\n\n${compiledMdx}`;
 }
 
 export default IntroLesson;

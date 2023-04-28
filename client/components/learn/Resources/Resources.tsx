2fc699c910f4b280c9da4c4e38d3cd372644bb5f
Alexandru Pavaloi <a.pavaloi@gmail.com>
2023-04-28 16:40:41 +0300

    Translate the resources page to english (#988)

--- client/components/learn/Resources/Resources.tsx
+++ client/components/learn/Resources/Resources.tsx
@@ -1,97 +1,38 @@
-import React from 'react';
+import List from '~/components/List';
 import { RESOURCES, ResourceCategory } from './resources-model';
 import ResourcePreview from './ResourcePreview/ResourcePreview';
-import List from '~/components/List';
+import { Trans, useTranslation } from '~/services/typesafeNextTranslate';
 
 import styles from './Resources.module.scss';
 
 function Resources() {
-  const sections = [{
-    id: ResourceCategory.ESSENTIALS,
-    title: 'Esențiale',
-    description: 'Noi le avem mereu deschise într-un tab. Tu?',
-  }, {
-    id: ResourceCategory.IMAGES_VIDEOS,
-    title: 'Imagini și Video-uri',
-    description: 'Vin cu o licență deschisă deci let poți folosi în orice proiect.',
-  },
-  {
-    id: ResourceCategory.VECTOR,
-    title: 'Ilustrații vectoriale',
-    description: 'Te ajută să faci site-ul mai prietenos și au o licență deschisă pentru a le folosi fără probleme.',
-  },
-  {
-    id: ResourceCategory.ICONS,
-    title: 'Iconițe',
-    description: 'Te ajută să faci interfața mai intuitivă, mai ales pe mobil unde nu avem chiar așa de mult spațiu.',
-  },
-  {
-    id: ResourceCategory.CSS_SPINNERS,
-    title: 'CSS Spiners',
-    description: 'Inspirație pentru loaderele din proiectele tale.',
-  }, {
-    id: ResourceCategory.TUTORIALS,
-    title: 'Tutoriale interactive',
-    description: 'Sunt multe tutoriale pe net, dar acestea ne-au atras atenția căci fac învățarea mai interactivă.',
-  }, {
-    id: ResourceCategory.BLOGS,
-    title: 'Blog-uri',
-    description: 'Pentru când vrei să te relaxezi citind despre ultimele noutăți.',
-  },
-  {
-    id: ResourceCategory.BOOKS,
-    title: 'Cărți',
-    description: 'Merită citite pe larg căci conțin o mulțime de informații care sigur te vor ajuta.',
-  },
-  {
-    id: ResourceCategory.BACKGROUNDS_AND_COLORS,
-    title: 'Background-uri & Culori',
-    description: 'Dacă echipa nu are un designer care să vină cu sugestii, te poți inspira de aici :)',
-  },
-  {
-    id: ResourceCategory.PERFORMANCE,
-    title: 'Performanță',
-    description: 'Asigură-te că aplicație rămâne performantă și respectă bunele practici.',
-  },
-  {
-    id: ResourceCategory.FONTS,
-    title: 'Fonts',
-    description: 'Adaugă mai multă personalitate site-ului tău schimbând fonturile.',
-  },
-  {
-    id: ResourceCategory.EXTRA,
-    title: 'Extra',
-    description: 'Îți îmbunătățesc întreaga experiență ca și developer.',
-  }];
-
+  const { t } = useTranslation('common');
   return (
     <main className={styles['resources-page']}>
-      <h1> 📚 Resurse pentru fiecare FrontEnd developer </h1>
+      <h1> {t('Resources.title')} </h1>
       <div className={styles['resources-page__about']}>
         <p>
-          Sunt atât de multe resurse gratuite încât e greu să știi care sunt cele mai bune.
-          <br />
-          <br />
-          Așa că am pornit de la site-urile ce ne ajută zilnic în joburile
-          nostre de FrontEnd developeri, le-am păstrat doar pe cele mai utile
-          și am creat cu ele lista de mai jos.
+          <Trans
+            i18nKey={'common:Resources.description'}
+            components={[
+              <br key='0' />,
+              <br key='1' />,
+            ]}
+          />
         </p>
         <p>
-          Așteptăm și sugestiile tale, așa că dă-ne un semn
-          cu resursele care te ajută pe tine și le vom adăuga aici.
+          {t('Resources.suggestions')}
         </p>
       </div>
       <List as="ol" className={styles['category-list']}>
-        {sections.map(({ id, title, description }) => (
-          <li key={title} className={`${styles.category} relative`}>
+        {Object.values(ResourceCategory).map((id) => (
+          <li key={id} className={`${styles.category} relative`}>
             <h2 id={id}>
-              {title}
+              {t(`Resources.chapters.${id}.title`)}
             </h2>
-            {!!description && (
-              <p className={styles.category__description}>
-                { description}
-              </p>
-            )}
+            <p className={styles.category__description}>
+              {t(`Resources.chapters.${id}.description`)}
+            </p>
             <List as="ol" className={styles['resources-list']}>
               {RESOURCES[id].map((resource) => (
                 <li key={resource.title}>
--- pages/resurse.tsx
+++ pages/resurse.tsx
@@ -3,15 +3,17 @@ import Footer from '~/components/Footer';
 import Header from '~/components/Header';
 import Resources from '~/components/learn/Resources';
 import SEOTags from '~/components/SEOTags';
+import { useTranslation } from '~/services/typesafeNextTranslate';
 
 function ResourcesPage() {
+  const { t, lang } = useTranslation('common');
   return (
     <>
       <SEOTags
-        title="Resurse pentru fiecare FrontEnd developer | FrontEnd.ro"
+        title={`${t('Resources.title')} | FrontEnd.ro`}
         shareImage={`${process.env.CLOUDFRONT_PUBLIC}/public/seo/resources.jpg`}
-        description="Am pornit de la site-urile ce ne ajută zilnic în joburile nostre de FrontEnd developeri, le-am păstrat doar pe cele mai utile și am creat cu ele lista de aici."
-        url="https://FrontEnd.ro/resurse"
+        description={t('Resources.seo_description')}
+        url={ lang === 'ro' ? "https://FrontEnd.ro/resurse" : "https://FrontendCademy.com/resurse"}
       />
       <Header withNavMenu />
       <Resources />
